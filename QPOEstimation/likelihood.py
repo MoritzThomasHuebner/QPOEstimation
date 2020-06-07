2a01ec790bcfa24429b4b94fc5f5314e97502d9b
Moritz Huebner <email@moritz-huebner.de>
2020-06-08 01:11:52 +1000

    Added some properties

--- QPOEstimation/likelihood.py
+++ QPOEstimation/likelihood.py
@@ -18,27 +18,57 @@ def lorentzian(frequencies, amplitude, central_frequency, width):
     return amplitude * (width ** 2 / ((frequencies - central_frequency) ** 2 + width ** 2)) / np.pi / width
 
 
+class ParameterAccessor(object):
+    def __init__(self, parameter_name):
+        self.parameter_name = parameter_name
+
+    def __get__(self, instance, owner):
+        return instance.parameters[self.parameter_name]
+
+    def __set__(self, instance, value):
+        instance.parameters[self.parameter_name] = value
+
+
 class QPLikelihood(Likelihood):
 
     VALID_NOISE_MODELS = ['red_noise', 'broken_power_law']
+    alpha = ParameterAccessor('alpha')
+    alpha_1 = ParameterAccessor('alpha_1')
+    alpha_2 = ParameterAccessor('alpha_2')
+    log_beta = ParameterAccessor('log_beta')
+    log_sigma = ParameterAccessor('log_sigma')
+    rho = ParameterAccessor('rho')
+    delta = ParameterAccessor('delta')
+    log_amplitude = ParameterAccessor('log_amplitude')
+    log_width = ParameterAccessor('log_width')
+    central_frequency = ParameterAccessor('central_frequency')
 
     def __init__(self, frequencies, amplitudes, frequency_mask, noise_model='red_noise'):
         super(QPLikelihood, self).__init__(
-            parameters=dict(alpha=0, alpha_1=0, alpha_2=0, beta=0, sigma=0, delta=0, rho=0,
-                            amplitude=0, width=1, central_frequency=127))
+            parameters=dict(alpha=0, alpha_1=0, alpha_2=0, log_beta=0, log_sigma=0, delta=0, rho=0,
+                            log_amplitude=0, log_width=0, central_frequency=127))
         self.frequencies = frequencies
         self.amplitudes = amplitudes
         self.frequency_mask = frequency_mask
         self.noise_model = noise_model
 
     @property
+    def frequencies(self):
+        return self._frequencies[self.frequency_mask]
+
+    @frequencies.setter
+    def frequencies(self, frequencies):
+        self._frequencies = frequencies
+
+    @property
     def psd(self):
         if self.noise_model == 'red_noise':
-            return red_noise(frequencies=self.frequencies[self.frequency_mask], alpha=self.alpha, beta=self.beta) \
-                   + white_noise(frequencies=self.frequencies[self.frequency_mask], sigma=self.sigma)
+            return red_noise(frequencies=self.frequencies, alpha=self.alpha, beta=self.beta) \
+                   + white_noise(frequencies=self.frequencies, sigma=self.sigma)
         elif self.noise_model == 'broken_power_law':
-            return broken_power_law_noise(frequencies=self.frequencies[self.frequency_mask], alpha_1=self.alpha_1, alpha_2=self.alpha_2, beta=self.beta, delta=self.delta, rho=self.rho) \
-                   + white_noise(frequencies=self.frequencies[self.frequency_mask], sigma=self.sigma)
+            return broken_power_law_noise(frequencies=self.frequencies, alpha_1=self.alpha_1,
+                                          alpha_2=self.alpha_2, beta=self.beta, delta=self.delta, rho=self.rho) \
+                   + white_noise(frequencies=self.frequencies, sigma=self.sigma)
 
     @property
     def noise_model(self):
@@ -53,47 +83,23 @@ class QPLikelihood(Likelihood):
 
     @property
     def lorentzian(self):
-        return lorentzian(self.frequencies[self.frequency_mask], self.amplitude, self.central_frequency, self.width)
-
-    @property
-    def alpha(self):
-        return self.parameters['alpha']
-
-    @property
-    def alpha_1(self):
-        return self.parameters['alpha_1']
-
-    @property
-    def alpha_2(self):
-        return self.parameters['alpha_2']
+        return lorentzian(self.frequencies, self.amplitude, self.central_frequency, self.width)
 
     @property
     def beta(self):
-        return self.parameters['beta']
-
-    @property
-    def delta(self):
-        return self.parameters['delta']
-
-    @property
-    def rho(self):
-        return self.parameters['rho']
+        return np.exp(self.log_beta)
 
     @property
     def sigma(self):
-        return self.parameters['sigma']
+        return np.exp(self.log_sigma)
 
     @property
     def amplitude(self):
-        return self.parameters['amplitude']
+        return np.exp(self.log_amplitude)
 
     @property
     def width(self):
-        return self.parameters['width']
-
-    @property
-    def central_frequency(self):
-        return self.parameters['central_frequency']
+        return np.exp(self.log_width)
 
     @property
     def model(self):
@@ -108,5 +114,23 @@ class QPLikelihood(Likelihood):
         return np.sum(-self.residual ** 2 / psd / 2 - np.log(2 * np.pi * psd) / 2)
 
 
-class ParameterAccessor(object):
-    pass
+class PeriodogramQPOLikelihood(QPLikelihood):
+
+    def __init__(self, frequencies, periodogram, frequency_mask, noise_model='red_noise'):
+        super(PeriodogramQPOLikelihood, self).__init__(frequencies=frequencies, frequency_mask=frequency_mask,
+                                                       noise_model=noise_model, amplitudes=None)
+        del self.amplitudes
+        self.frequencies = frequencies
+        self._periodogram = periodogram
+        self.frequency_mask = frequency_mask
+        self.noise_model = noise_model
+
+    @property
+    def periodogram(self):
+        return self._periodogram[self.frequency_mask]
+
+    def log_likelihood(self):
+        psd = self.psd + self.model
+        return -np.sum(np.log(psd) + self.periodogram / psd)
+
+
