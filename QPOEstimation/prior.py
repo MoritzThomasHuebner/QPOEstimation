2c5ab1b2ebe32bfe717de7c3793bf6c3c797b759
Moritz Huebner <email@moritz-huebner.de>
2020-06-23 17:32:38 +1000

    Added automated prior generation

--- QPOEstimation/prior.py
+++ QPOEstimation/prior.py
@@ -1,6 +1,5 @@
 import bilby
 import numpy as np
-from copy import deepcopy
 
 
 def get_red_noise_prior():
@@ -47,3 +46,47 @@ def get_full_prior(noise_model='red_noise', frequencies=None):
     for key, value in noise_prior.items():
         prior[key] = value
     return prior
+
+
+class SlabSpikePrior(bilby.core.prior.Prior):
+
+    def __init__(self, name=None, latex_label=None, unit=None, minimum=0.,
+                 maximum=1., spike_height=0.5, check_range_nonzero=True, boundary=None):
+        super().__init__(name=name, latex_label=latex_label, unit=unit, minimum=minimum,
+                         maximum=maximum, check_range_nonzero=check_range_nonzero, boundary=boundary)
+        self.spike_loc = minimum
+        self.spike_height = spike_height
+
+    def rescale(self, val):
+        self.test_valid_for_rescaling(val)
+        if val >= self.spike_height:
+            return self.minimum + (val - self.spike_height) / (1 - self.spike_height) * (self.maximum - self.minimum)
+        else:
+            return self.minimum
+
+    def prob(self, val):
+        return ((val >= self.minimum) & (val <= self.maximum)) * (self.spike_height + (1 - self.spike_height) / (self.maximum - self.minimum))
+
+    def ln_prob(self, val):
+        return np.log(self.prob(val))
+
+
+def generate_qpo_prior_dict(t_start, t_end, max_burst_amplitude=1e5, max_n_bursts=1, max_qpo_amplitude=1e5,
+                            max_n_qpos=1, max_background=1e4, max_frequency=1e3, ):
+    max_sigma = t_end - t_start
+    T = max_sigma
+    priors = bilby.core.prior.ConditionalPriorDict(dict())
+    priors['background_rate'] = bilby.core.prior.LogUniform(minimum=1, maximum=max_background, name='background')
+    for i in range(max_n_bursts):
+        priors[f'amplitude_{i}'] = SlabSpikePrior(minimum=0, maximum=max_burst_amplitude, spike_height=1 - 1/(i + i), name=f'amplitude_{i}')
+        priors[f't_max_{i}'] = bilby.core.prior.Uniform(minimum=t_start, maximum=t_end, name=f't_max_{i}')
+        priors[f'sigma_{i}'] = bilby.core.prior.LogUniform(minimum=1e-4, maximum=max_sigma, name=f'sigma_{i}')
+        priors[f'skewness_{i}'] = bilby.core.prior.Uniform(minimum=0, maximum=100, name=f's_{i}')
+    for i in range(max_n_qpos):
+        priors['amplitude_qpo'] = SlabSpikePrior(minimum=0, maximum=max_qpo_amplitude, name='amplitude_qpo')
+        # priors['offset'] = bilby.core.prior.LogUniform(minimum=1/nbins/T, maximum=1e9, name='offset')
+        # priors['phase'] = bilby.core.prior.Uniform(minimum=0, maximum=2*np.pi, name='phase')
+        priors['frequency'] = bilby.core.prior.LogUniform(minimum=10 / T, maximum=max_frequency, name='frequency')
+        priors['t_0'] = bilby.core.prior.Uniform(minimum=t_start, maximum=t_end, name='t_0')
+        priors['decay_time'] = bilby.core.prior.LogUniform(minimum=1/max_frequency, maximum=T, name='decay_time')
+        priors['phase'] = bilby.core.prior.DeltaFunction(peak=0, name='phase')
