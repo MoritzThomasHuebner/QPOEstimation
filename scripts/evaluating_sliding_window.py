365e3e6cab9e101f6e433cebbb66ba572bfe6c4f
Moritz Huebner <email@moritz-huebner.de>
2020-08-18 03:03:52 +1000

    set up evaluation script

--- scripts/evaluating_sliding_window.py
+++ scripts/evaluating_sliding_window.py
@@ -1,13 +1,11 @@
 import bilby
-im
 for i in range(31):
     mean_log_bf = 0
     for j in range(9):
         try:
             res = bilby.result.read_in_result(f"sliding_window/period_{j}/{i}_result.json")
             mean_log_bf += res.log_bayes_factor
-            if res.log_bayes_factor > 4:
-                print(f"{j} {i}: {res.log_bayes_factor}")
+            print(f"{j} {i}: {res.log_bayes_factor}")
         except Exception:
             pass
-    # print(f"{i}: {mean_log_bf}")
+    print(f"{i}: {mean_log_bf}")
