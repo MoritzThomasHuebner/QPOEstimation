69c8f05fd843bc0d548f49ee846e0c83e54c2e19
Moritz Huebner <email@moritz-huebner.de>
2020-08-20 18:52:10 +1000

    Runs for 2 qpos

--- scripts/sliding_window.py
+++ scripts/sliding_window.py
@@ -8,12 +8,13 @@ from celerite import terms
 
 from QPOEstimation.stabilisation import bar_lev
 from QPOEstimation.model.series import *
+from QPOEstimation.prior.slabspike import SlabSpikePrior
 from QPOEstimation.likelihood import CeleriteLikelihood, QPOTerm
 
 run_id = int(sys.argv[1])
 period_number = int(sys.argv[2])
-# run_id = 19
-# period_number = 6
+# run_id = 20
+# period_number = 3
 
 fits_data = fits.open('data/SGR_1806_20/event_4ms.lc.gz')
 times = fits_data[1].data["TIME"]
@@ -73,7 +74,7 @@ c = c.astype(int)
 # alpha = 0.08
 # background = two_sided_exponential_smoothing(background_c, alpha)
 
-outdir = f"sliding_window/period_{period_number}"
+outdir = f"sliding_window/period_{period_number}/two_qpo"
 
 # truncate background
 # truncated_background_start = np.where(background_t == t[0])[0][0]
@@ -91,13 +92,17 @@ stabilised_variance = np.ones(len(stabilised_counts))
 Q = 1.0 / np.sqrt(2.0)
 w0 = 3.0
 S0 = np.var(stabilised_counts) / (w0 * Q)
-bounds = dict(log_S0=(-15, 15), log_Q=(-15, 15), log_omega0=(-15, 15))
-burst_shape_term = terms.SHOTerm(log_S0=np.log(S0), log_Q=np.log(Q), log_omega0=np.log(w0), bounds=bounds)
+# bounds = dict(log_S0=(-15, 40), log_Q=(np.log(1/np.sqrt(2)), np.log(1/np.sqrt(2))), log_omega0=(-40, 15))
+burst_shape_term = terms.SHOTerm(log_S0=np.log(S0), log_Q=np.log(Q), log_omega0=np.log(w0))#, bounds=bounds)
 kernel = burst_shape_term
 
-bounds = dict(log_a=(0, 50), log_b=(0, 50), log_c=(-3.0, 3.0), log_P=(-15, 15))
-qpo_term = QPOTerm(log_a=0.1, log_b=0.5, log_c=-0.01, log_P=0.0, bounds=bounds)
+# bounds = dict(log_a=(-15, 15), log_b=(-15, 15), log_c=(-6.0, 6.0), log_P=(-4.85, -2.0))
+qpo_term = QPOTerm(log_a=0.1, log_b=0.5, log_c=-0.01, log_P=-3)#, bounds=bounds)
 kernel += qpo_term
+qpo_term = QPOTerm(log_a=0.1, log_b=0.5, log_c=-0.01, log_P=-3)
+kernel += qpo_term
+
+# qpo_term = QPOTerm(log_a=0.1, log_b=0.5, log_c=-0.01, log_P=0.0, bounds=bounds)
 
 params_dict = kernel.get_parameter_dict()
 for param in params_dict:
@@ -114,39 +119,51 @@ print("parameter_vector:\n{0}\n".format(gp.get_parameter_vector()))
 print("parameter_bounds:\n{0}\n".format(gp.get_parameter_bounds()))
 print(gp.get_parameter_dict())
 
-priors = bilby.core.prior.PriorDict()
+def log_P_fraction(sample):
+    res = deepcopy(sample)
+    res['log_P_fraction'] = sample['kernel:terms[1]:log_P']/sample['kernel:terms[2]:log_P']
+    return res
+
+
+priors = bilby.core.prior.PriorDict(conversion_function=log_P_fraction)
 priors['kernel:terms[0]:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=40, name='terms[0]:log_S0')
 priors['kernel:terms[0]:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1/np.sqrt(2)), name='terms[0]:log_Q')
 priors['kernel:terms[0]:log_omega0'] = bilby.core.prior.Uniform(minimum=-40, maximum=15, name='terms[0]:log_omega0')
 # priors['kernel:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=40, name='log_S0')
 # priors['kernel:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1/np.sqrt(2)), name='log_Q')
 # priors['kernel:log_omega0'] = bilby.core.prior.Uniform(minimum=-40, maximum=15, name='log_omega0')
-priors['kernel:terms[1]:log_a'] = bilby.core.prior.Uniform(minimum=-5, maximum=15, name='terms[1]:log_a')
-priors['kernel:terms[1]:log_b'] = bilby.core.prior.Uniform(minimum=-5, maximum=5, name='terms[1]:log_b')
-priors['kernel:terms[1]:log_c'] = bilby.core.prior.Uniform(minimum=-6, maximum=6, name='terms[1]:log_c')
-priors['kernel:terms[1]:log_P'] = bilby.core.prior.Uniform(minimum=-4.85, maximum=-2.3, name='terms[1]:log_P')
-
+priors['kernel:terms[1]:log_a'] = SlabSpikePrior(minimum=-5, maximum=15, spike_loc=-5, spike_height=0.2, name='terms[1]:log_a')
+priors['kernel:terms[1]:log_b'] = bilby.core.prior.DeltaFunction(peak=-10, name='terms[1]:log_b')
+priors['kernel:terms[1]:log_c'] = bilby.core.prior.Uniform(minimum=-6, maximum=3.5, name='terms[1]:log_c')
+priors['kernel:terms[1]:log_P'] = bilby.core.prior.Uniform(minimum=-4.16, maximum=-2.0, name='terms[1]:log_P')
+priors['kernel:terms[2]:log_a'] = SlabSpikePrior(minimum=-5, maximum=15, spike_loc=-5, spike_height=0.2, name='terms[2]:log_a')
+# priors['kernel:terms[2]:log_a'] = bilby.core.prior.Uniform(minimum=-5, maximum=15, name='terms[2]:log_a')
+priors['kernel:terms[2]:log_b'] = bilby.core.prior.DeltaFunction(peak=-10, name='terms[2]:log_b')
+priors['kernel:terms[2]:log_c'] = bilby.core.prior.Uniform(minimum=-6, maximum=3.5, name='terms[2]:log_c')
+priors['kernel:terms[2]:log_P'] = bilby.core.prior.Uniform(minimum=-4.85, maximum=-4.16, name='terms[2]:log_P')
+priors['log_P_fraction'] = bilby.core.prior.Constraint(minimum=0, maximum=1)
 
 likelihood = CeleriteLikelihood(gp=gp, y=stabilised_counts)
-label = f'{run_id}_qpo'
+label = f'{run_id}_two_qpo'
 # outdir = "test"
 result = bilby.run_sampler(likelihood=likelihood, priors=priors, outdir=outdir,
                            label=label, sampler='dynesty', nlive=300, sample='rwalk', resume=False)
 result.plot_corner()
+# result = bilby.result.read_in_result(outdir=outdir, label=label)
 
+for term in [1, 2]:
+    frequency_samples = []
+    for i, sample in enumerate(result.posterior.iloc):
+        frequency_samples.append(1 / np.exp(sample[f'kernel:terms[{term}]:log_P']))
 
-# frequency_samples = []
-# for i, sample in enumerate(result.posterior.iloc):
-#     frequency_samples.append(1 / np.exp(sample['kernel:terms[1]:log_P']))
-#
-# plt.hist(frequency_samples, bins="fd", density=True)
-# plt.xlabel('frequency [Hz]')
-# plt.ylabel('normalised PDF')
-# median = np.median(frequency_samples)
-# percentiles = np.percentile(frequency_samples, [16, 84])
-# plt.title(f"{np.mean(frequency_samples):.2f} + {percentiles[1] - median:.2f} / - {- percentiles[0] + median:.2f}")
-# plt.savefig(f"{outdir}/frequency_posterior_{label}")
-# plt.clf()
+    plt.hist(frequency_samples, bins="fd", density=True)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('normalised PDF')
+    median = np.median(frequency_samples)
+    percentiles = np.percentile(frequency_samples, [16, 84])
+    plt.title(f"{np.mean(frequency_samples):.2f} + {percentiles[1] - median:.2f} / - {- percentiles[0] + median:.2f}")
+    plt.savefig(f"{outdir}/frequency_posterior_{label}_{term}")
+    plt.clf()
 
 
 x = np.linspace(t[0], t[-1], 5000)
