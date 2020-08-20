19093fa863a8f16fb0872a3dc1cef1b1f511015a
Moritz Huebner <email@moritz-huebner.de>
2020-08-20 21:24:28 +1000

    Fixed analysis script

--- scripts/evaluating_sliding_window.py
+++ scripts/evaluating_sliding_window.py
@@ -2,7 +2,7 @@ import bilby
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-matplotlib.use("Qt5Agg")
+# matplotlib.use("Qt5Agg")
 from copy import deepcopy
 segments = np.arange(0, 31)
 mean_log_bfs = []
@@ -34,9 +34,9 @@ for j in range(9):
     log_bfs_two_qpo = []
     for i in range(31):
         try:
-            res_no_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/two_qpo/{i}_no_qpo_result.json")
-            res_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/two_qpo/{i}_qpo_result.json")
-            res_two_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/two_qpo/{i}_two_qpo_result.json")
+            res_no_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/{i}_no_qpo_result.json")
+            res_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/{i}_one_qpo_result.json")
+            res_two_qpo = bilby.result.read_in_result(f"sliding_window/period_{j}/{i}_two_qpo_result.json")
             log_bf_one_qpo = res_qpo.log_evidence - res_no_qpo.log_evidence
             log_bfs_one_qpo.append(log_bf_one_qpo)
             log_bf_two_qpo = res_two_qpo.log_evidence - res_no_qpo.log_evidence
@@ -53,6 +53,7 @@ for j in range(9):
     plt.xlabel("Data segment")
     plt.ylabel("ln BF")
     plt.savefig("one_qpo_log_bfs")
+    plt.clf()
 
 for j in range(9):
     plt.plot(segments, period_two_log_bf_data[j], label=f'period_{j}')
@@ -60,3 +61,4 @@ for j in range(9):
     plt.xlabel("Data segment")
     plt.ylabel("ln BF")
     plt.savefig("two_qpo_log_bfs")
+    plt.clf()
