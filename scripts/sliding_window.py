5484427f0770e255ba21462871c65d568b7c383d
Moritz Huebner <email@moritz-huebner.de>
2020-09-08 12:24:00 +1000

    ads

--- scripts/sliding_window.py
+++ scripts/sliding_window.py
@@ -21,15 +21,16 @@ run_id = None
 period_number = None
 # n_qpos = 1
 
-# candidate_id = int(sys.argv[1])
-# n_qpos = int(sys.argv[2])
+candidate_id = int(sys.argv[1])
+n_qpos = int(sys.argv[2])
+model_id = int(sys.argv[3])
 
-n_qpos = 0
-candidate_id = 0
-model_id = 1
+# n_qpos = 1
+# candidate_id = 0
+# model_id = 2
 
 # data = np.loadtxt(f'data/sgr1806_256Hz.dat')
-data = np.loadtxt(f'data/sgr1806_1024Hz.dat')
+data = np.loadtxt(f'data/sgr1806_64Hz.dat')
 times = data[:, 0]
 counts = data[:, 1]
 
@@ -37,6 +38,13 @@ likelihood_models = ['gaussian_process', 'periodogram', 'poisson']
 likelihood_model = likelihood_models[model_id]
 candidates_run = True
 band = 'below_16Hz'
+
+if likelihood_model in [likelihood_models[0], likelihood_models[2]]:
+    data = np.loadtxt(f'data/sgr1806_64Hz.dat')
+elif likelihood_model == likelihood_models[1]:
+    data = np.loadtxt(f'data/sgr1806_1024Hz.dat')
+times = data[:, 0]
+counts = data[:, 1]
 
 
 if candidates_run:
@@ -73,18 +81,18 @@ c = c.astype(int)
 
 if candidates_run:
     if n_qpos == 0:
-        outdir = f"sliding_window_{band}_candidates/no_qpo/results"
+        outdir = f"sliding_window_{band}_candidates/no_qpo"
     elif n_qpos == 1:
-        outdir = f"sliding_window_{band}_candidates/one_qpo/results"
+        outdir = f"sliding_window_{band}_candidates/one_qpo"
     else:
-        outdir = f"sliding_window_{band}_candidates/two_qpo/results"
+        outdir = f"sliding_window_{band}_candidates/two_qpo"
 else:
     if n_qpos == 0:
-        outdir = f"sliding_window_{band}/period_{period_number}/no_qpo/results"
+        outdir = f"sliding_window_{band}/period_{period_number}/no_qpo"
     elif n_qpos == 1:
-        outdir = f"sliding_window_{band}/period_{period_number}/one_qpo/results"
+        outdir = f"sliding_window_{band}/period_{period_number}/one_qpo"
     else:
-        outdir = f"sliding_window_{band}/period_{period_number}/two_qpo/results"
+        outdir = f"sliding_window_{band}/period_{period_number}/two_qpo"
 
 stabilised_counts = bar_lev(c)
 stabilised_variance = np.ones(len(stabilised_counts))
@@ -191,11 +199,10 @@ elif likelihood_model == likelihood_models[2]:
 # try:
 # result = bilby.result.read_in_result(outdir=outdir, label=label)
 # except Exception:
-result = bilby.run_sampler(likelihood=likelihood, priors=priors, outdir=outdir,
+result = bilby.run_sampler(likelihood=likelihood, priors=priors, outdir=f"{outdir}/results",
                            label=label, sampler='dynesty', nlive=200, sample='rwalk',
                            resume=False, clean=True)
 result.plot_corner(outdir=f"{outdir}/corner")
-print(result.log_evidence)
 if likelihood_model == likelihood_models[0]:
     for term in [1, 2]:
         try:
@@ -235,7 +242,7 @@ if likelihood_model == likelihood_models[0]:
     plt.xlabel("time [s]")
     plt.ylabel("variance stabilised data")
     # plt.show()
-    plt.savefig(f"{outdir}/fits/max_like_fit_{label}")
+    plt.savefig(f"{outdir}/fits/{label}_max_like_fit")
     plt.clf()
 
     pred_mean, pred_var = gp.predict(stabilised_counts, t, return_var=True)
@@ -243,7 +250,7 @@ if likelihood_model == likelihood_models[0]:
     plt.fill_between(t, 1, -1, color=color, alpha=0.3, edgecolor="none")
     plt.xlabel("time [s]")
     plt.ylabel("stabilised residuals")
-    plt.savefig(f"{outdir}/fits/max_like_fit_residuals_{label}")
+    plt.savefig(f"{outdir}/fits/{label}_max_like_fit_residuals")
     plt.clf()
 elif likelihood_model == likelihood_models[1]:
     if n_qpos > 0:
@@ -285,7 +292,7 @@ elif likelihood_model == likelihood_models[2]:
     for i in range(10):
         parameters = result.posterior.iloc[np.random.randint(len(result.posterior))]
         plt.plot(t, sine_func(t, **parameters) + background_estimate, color='r', alpha=0.2)
-    plt.savefig(f'{outdir}/fits/max_like_fit_poisson{label}.png')
+    plt.savefig(f'{outdir}/fits/{label}_max_like_fit.png')
     plt.xlabel("time [s]")
     plt.ylabel("counts")
     plt.legend()
@@ -297,13 +304,18 @@ elif likelihood_model == likelihood_models[2]:
     plt.xlabel("time [s]")
     plt.ylabel("residuals")
     plt.legend()
-    plt.savefig(f'{outdir}/fits/max_like_fit_poisson_residuals{label}.png')
+    plt.savefig(f'{outdir}/fits/{label}_max_like_fit_residuals.png')
     plt.show()
 
 
 
 # clean up
-
+for extension in ['_checkpoint_run.png', '_checkpoint_stats.png', '_checkpoint_trace.png', #'_corner.png',
+                  '_dynesty.pickle', '_resume.pickle', '_result.json.old', '_samples.dat']:
+    try:
+        os.remove(f"{outdir}/results/{label}{extension}")
+    except Exception:
+        pass
 # max_like_params = dict(amplitude=0, frequency=29, phase=0, mu=241.05, sigma=0.1, elevation=1, c_0=1, c_1=-0, c_2=1, c_3=0, c_4=-0.0)
 
 # plt.plot(t, c, label='Data')
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,12 +3,13 @@
 #SBATCH --job-name=test
 #
 #SBATCH --ntasks=1
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=1G
 
 #srun python sliding_window.py ${1} ${2} 0
 #srun python sliding_window.py ${1} ${2} 1
 
 # Candidate runs
-srun python sliding_window.py ${1} 0
-srun python sliding_window.py ${1} 1
+srun python sliding_window.py ${1} 0 1
+srun python sliding_window.py ${1} 1 1
+srun python sliding_window.py ${2} 1 2
