3017d4a021c1838389131f6fd684222c34f28389
Moritz Huebner <email@moritz-huebner.de>
2020-09-24 01:09:44 +1000

    Set up for more te

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -13,10 +13,10 @@
 #srun python sliding_window.py ${1} ${2} 1 0
 
 # Candidate runs
-srun python sliding_window.py ${1} 0 0
+#srun python sliding_window.py ${1} 0 0
 srun python sliding_window.py ${1} 1 0
 #srun python sliding_window.py ${1} 2 0
-srun python sliding_window.py ${1} 0 1
-srun python sliding_window.py ${1} 1 1
-srun python sliding_window.py ${1} 0 2
-srun python sliding_window.py ${1} 1 2
+#srun python sliding_window.py ${1} 0 1
+#srun python sliding_window.py ${1} 1 1
+#srun python sliding_window.py ${1} 0 2
+#srun python sliding_window.py ${1} 1 2
