5840362500979574e7d1684feeebe65126ca063e
Moritz Huebner <email@moritz-huebner.de>
2020-10-06 17:06:33 +1100

    Made some changes

--- scripts/evaluating_sliding_window.py
+++ scripts/evaluating_sliding_window.py
@@ -32,12 +32,12 @@ for period in range(n_periods):
         try:
             res_no_qpo = bilby.result.read_in_result(f"{outdir}/period_{period}/no_qpo/results/{run_id}_result.json")
             res_one_qpo = bilby.result.read_in_result(f"{outdir}/period_{period}/one_qpo/results/{run_id}_result.json")
-            res_no_qpo_whittle = bilby.result.read_in_result(f"{outdir}/period_{period}/no_qpo/results/{run_id}_whittle_result.json")
-            res_one_qpo_whittle = bilby.result.read_in_result(f"{outdir}/period_{period}/one_qpo/results/{run_id}_whittle_result.json")
+            # res_no_qpo_whittle = bilby.result.read_in_result(f"{outdir}/period_{period}/no_qpo/results/{run_id}_whittle_result.json")
+            # res_one_qpo_whittle = bilby.result.read_in_result(f"{outdir}/period_{period}/one_qpo/results/{run_id}_whittle_result.json")
             # res_two_qpo = bilby.result.read_in_result(f"sliding_window/period_{period}/two_qpo/{run_id}_result.json")
             # res_two_qpo = bilby.result.read_in_result(f"sliding_window/period_{period}/two_qpo/{run_id}_two_qpo_result.json")
             log_bf_one_qpo = res_one_qpo.log_evidence - res_no_qpo.log_evidence
-            log_bf_one_qpo_whittle = res_one_qpo_whittle.log_evidence - res_no_qpo_whittle.log_evidence
+            # log_bf_one_qpo_whittle = res_one_qpo_whittle.log_evidence - res_no_qpo_whittle.log_evidence
             # log_bf_two_qpo = res_two_qpo.log_evidence - res_no_qpo.log_evidence
             # max_likelihood_sample_one_qpo = res_one_qpo.posterior.iloc[-1]
             # mean_frequency.append(1 / np.exp(max_likelihood_sample_one_qpo[f'kernel:terms[1]:log_P']))
@@ -45,9 +45,9 @@ for period in range(n_periods):
             frequency_samples = 1 / np.exp(log_P_samples)
             mean_frequency.append(np.mean(frequency_samples))
             std_frequency.append(np.std(frequency_samples))
-            frequency_samples_whittle = np.array(res_one_qpo_whittle.posterior['central_frequency'])
-            mean_frequency_whittle.append(np.mean(frequency_samples_whittle))
-            std_frequency_whittle.append(np.std(frequency_samples_whittle))
+            # frequency_samples_whittle = np.array(res_one_qpo_whittle.posterior['central_frequency'])
+            # mean_frequency_whittle.append(np.mean(frequency_samples_whittle))
+            # std_frequency_whittle.append(np.std(frequency_samples_whittle))
         except Exception as e:
             print(e)
             log_bf_one_qpo = np.nan
@@ -57,7 +57,7 @@ for period in range(n_periods):
             mean_frequency_whittle.append(np.nan)
             std_frequency_whittle.append(np.nan)
         log_bfs_one_qpo.append(log_bf_one_qpo)
-        log_bfs_one_qpo_whittle.append(log_bf_one_qpo_whittle)
+        # log_bfs_one_qpo_whittle.append(log_bf_one_qpo_whittle)
         # log_bfs_two_qpo.append(log_bf_two_qpo)
         print(f"{period} {run_id} one qpo: {log_bf_one_qpo}")
         # print(f"{period} {run_id} two qpo: {log_bf_two_qpo}")
@@ -91,30 +91,30 @@ for period in range(n_periods):
     plt.clf()
 
 
-    fig, ax1 = plt.subplots()
-    color = 'tab:red'
-    ax1.set_xlabel('segment start time [s]')
-    ax1.set_ylabel('ln BF', color=color)
-    ax1.plot(start_times, log_bfs_one_qpo_whittle, color=color, ls='solid', label='One QPO')
-    # ax1.plot(start_times, log_bfs_two_qpo, color=color, ls='dotted', label='Two QPOs')
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-    color = 'tab:blue'
-    ax2.set_ylabel('frequency [Hz]', color=color)  # we already handled the x-label with ax1
-    ax2.plot(start_times, mean_frequency_whittle, color=color)
-    mean_frequency_whittle = np.array(mean_frequency_whittle)
-    std_frequency_whittle = np.array(std_frequency_whittle)
-    plt.fill_between(start_times, mean_frequency_whittle + std_frequency_whittle,
-                     mean_frequency_whittle - std_frequency_whittle,
-                     color=color, alpha=0.3, edgecolor="none")
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    ax1.legend()
-    plt.savefig(f'{outdir}/log_bfs_period_{period}_whittle')
-    plt.clf()
+    # fig, ax1 = plt.subplots()
+    # color = 'tab:red'
+    # ax1.set_xlabel('segment start time [s]')
+    # ax1.set_ylabel('ln BF', color=color)
+    # ax1.plot(start_times, log_bfs_one_qpo_whittle, color=color, ls='solid', label='One QPO')
+    # # ax1.plot(start_times, log_bfs_two_qpo, color=color, ls='dotted', label='Two QPOs')
+    # ax1.tick_params(axis='y', labelcolor=color)
+    #
+    # ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    #
+    # color = 'tab:blue'
+    # ax2.set_ylabel('frequency [Hz]', color=color)  # we already handled the x-label with ax1
+    # ax2.plot(start_times, mean_frequency_whittle, color=color)
+    # mean_frequency_whittle = np.array(mean_frequency_whittle)
+    # std_frequency_whittle = np.array(std_frequency_whittle)
+    # plt.fill_between(start_times, mean_frequency_whittle + std_frequency_whittle,
+    #                  mean_frequency_whittle - std_frequency_whittle,
+    #                  color=color, alpha=0.3, edgecolor="none")
+    # ax2.tick_params(axis='y', labelcolor=color)
+    #
+    # fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    # ax1.legend()
+    # plt.savefig(f'{outdir}/log_bfs_period_{period}_whittle')
+    # plt.clf()
     # period_one_log_bf_data.append(deepcopy(log_bfs_one_qpo))
     # period_two_log_bf_data.append(deepcopy(log_bfs_two_qpo))
 
