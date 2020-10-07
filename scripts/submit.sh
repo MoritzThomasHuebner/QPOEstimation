524dffa5bc10bd20c8cad1670c9e6461ce99d4a6
Moritz Huebner <email@moritz-huebner.de>
2020-10-08 01:13:01 +1100

    Made some changes

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -7,18 +7,18 @@
 #SBATCH --mem-per-cpu=1G
 
 
-#srun python sliding_window.py ${1} ${2} 0 0
-#srun python sliding_window.py ${1} ${2} 1 0
-#srun python sliding_window.py ${1} ${2} 0 1
-#srun python sliding_window.py ${1} ${2} 1 1
+srun python sliding_window.py ${1} ${2} 0 0
+srun python sliding_window.py ${1} ${2} 1 0
+srun python sliding_window.py ${1} ${2} 0 1
+srun python sliding_window.py ${1} ${2} 1 1
 
 # Candidate runs
 #srun python sliding_window.py ${1} ${2} 0 0
 #srun python sliding_window.py ${1} ${2} 1 0
 #srun python sliding_window.py ${1} 2 0
-srun python sliding_window.py ${1} 0 0
-srun python sliding_window.py ${1} 1 0
-srun python sliding_window.py ${1} 0 1
-srun python sliding_window.py ${1} 1 1
+#srun python sliding_window.py ${1} 0 0
+#srun python sliding_window.py ${1} 1 0
+#srun python sliding_window.py ${1} 0 1
+#srun python sliding_window.py ${1} 1 1
 #srun python sliding_window.py ${1} 0 2
 #srun python sliding_window.py ${1} 1 2
