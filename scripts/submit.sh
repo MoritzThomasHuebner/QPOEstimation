bcbd967ef13353835f00d4bbb6ee93dacd251be8
Moritz Huebner <email@moritz-huebner.de>
2020-10-09 19:19:15 +1100

    Refined argument parsing

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -7,14 +7,18 @@
 #SBATCH --mem-per-cpu=1G
 
 
-srun python sliding_window.py ${1} ${2} 0 0
-srun python sliding_window.py ${1} ${2} 1 0
-#srun python sliding_window.py ${1} ${2} 0 1
-#srun python sliding_window.py ${1} ${2} 1 1
+srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 0 --model gaussian_process --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model gaussian_process --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 0 --model periodogram --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model periodogram --band_minimum 10 --band_maximum 40
 
 # Candidate runs
-#srun python sliding_window.py ${1} ${2} 0 0
-#srun python sliding_window.py ${1} ${2} 1 0
+srun python sliding_window.py --candidate_run True --candidate_id ${2} --n_qpos 0 --model gaussian_process --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --candidate_run True --candidate_id ${2} --n_qpos 1 --model gaussian_process --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --candidate_run True --candidate_id ${2} --n_qpos 0 --model periodogram --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --candidate_run True --candidate_id ${2} --n_qpos 1 --model periodogram --band_minimum 10 --band_maximum 40
+
+
 #srun python sliding_window.py ${1} 2 0
 #srun python sliding_window.py ${1} 0 0
 #srun python sliding_window.py ${1} 1 0
