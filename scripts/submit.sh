b9772f7003cd1ff406915d8bb9260c88d6ad4bce
Moritz Huebner <email@moritz-huebner.de>
2020-10-13 21:48:19 +1100

    set up for miller candidate runs

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -13,15 +13,15 @@
 #srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model periodogram --band_minimum 10 --band_maximum 40
 
 # Candidate runs
-srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4}
-srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4}
+#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4}
+#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4}
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model periodogram --plot True --band_minimum 5 --band_maximum 16
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model periodogram --plot True --band_minimum 5 --band_maximum 16
 #
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model gaussian_process --plot True --band_minimum 10 --band_maximum 40
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model gaussian_process --plot True --band_minimum 10 --band_maximum 40
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model periodogram --plot True --band_minimum 10 --band_maximum 40
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model periodogram --plot True --band_minimum 10 --band_maximum 40
+srun python sliding_window.py --candidates_run True --miller_candidates True --candidate_id ${1} --n_qpos 0 --model gaussian_process --plot True
+srun python sliding_window.py --candidates_run True --miller_candidates True --candidate_id ${1} --n_qpos 1 --model gaussian_process --plot True
+srun python sliding_window.py --candidates_run True --miller_candidates True --candidate_id ${1} --n_qpos 0 --model periodogram --plot True
+srun python sliding_window.py --candidates_run True --miller_candidates True --candidate_id ${1} --n_qpos 1 --model periodogram --plot True
 #
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model gaussian_process --plot True --band_minimum 40 --band_maximum 128
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model gaussian_process --plot True --band_minimum 40 --band_maximum 128
