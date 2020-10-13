5dd81b2146ea255e750d0c31dd00ca2b940195c9
Moritz Huebner <email@moritz-huebner.de>
2020-10-13 16:07:19 +1100

    Various minor improvements

--- scripts/identify candidates.py
+++ scripts/identify candidates.py
@@ -6,7 +6,7 @@ import numpy as np
 n_periods = 47
 # band = '10_40Hz
 # band = '5_16Hz'
-band = '40_128Hz'
+band = '5_16Hz'
 # band = 'below_16Hz'
 
 Candidate = namedtuple('Candidate', ['period_number', 'index_range', 'start', 'stop', 'mean', 'std'])
@@ -20,12 +20,13 @@ def ranges(nums):
 candidates = []
 pulse_period = 7.56  # see papers
 time_offset = 20.0
+segment_step = 0.27
 
 for i in range(n_periods):
     log_bfs = np.loadtxt(f'sliding_window_{band}/log_bfs_period_one_qpo_{i}')
     mean_freqs = np.loadtxt(f'sliding_window_{band}/mean_frequencies_{i}')
     std_freqs = np.loadtxt(f'sliding_window_{band}/std_frequencies_{i}')
-    candidate_indices = np.where(np.logical_and(log_bfs > 8, std_freqs/mean_freqs < 1/4))[0]
+    candidate_indices = np.where(np.logical_and(log_bfs > 6, std_freqs/mean_freqs < 1/4))[0]
     # candidate_indices = np.where(log_bfs > 4)[0]
     rs = ranges(candidate_indices)
 
@@ -37,8 +38,8 @@ for i in range(n_periods):
             # middle = np.int((r[1] + r[0])/2)
             candidates.append(Candidate(
                 period_number=i, index_range=(r[0], r[1]),
-                start=time_offset + i * pulse_period + preferred_index * 0.27,
-                stop=time_offset + i * pulse_period + preferred_index * 0.27 + 1,
+                start=time_offset + i * pulse_period + preferred_index * segment_step,
+                stop=time_offset + i * pulse_period + preferred_index * segment_step + 1,
                 mean=mean_freqs[preferred_index], std=std_freqs[preferred_index]))
 
 
