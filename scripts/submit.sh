b4b9be1b3dbffd897671fbd42b33496db999272a
Moritz Huebner <email@moritz-huebner.de>
2020-10-14 20:24:45 +1100

    set up for miller candidate runs

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -7,14 +7,14 @@
 #SBATCH --mem-per-cpu=1G
 
 
-srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 0 --model ${3} --band_minimum ${4} --band_maximum ${5} --suffix _0.5s --segment_length 0.5
-srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model ${3} --band_minimum ${4} --band_maximum ${5} --suffix _0.5s --segment_length 0.5
+#srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 0 --model ${3} --band_minimum ${4} --band_maximum ${5} --suffix _0.5s --segment_length 0.5
+#srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model ${3} --band_minimum ${4} --band_maximum ${5} --suffix _0.5s --segment_length 0.5
 #srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 0 --model periodogram --band_minimum 10 --band_maximum 40  --suffix _0.5s --segment_length 0.5
 #srun python sliding_window.py --run_id ${1} --period_number ${2} --n_qpos 1 --model periodogram --band_minimum 10 --band_maximum 40  --suffix _0.5s --segment_length 0.5
 #
 # Candidate runs
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4} --suffix _0.5s --segment_length 0.5
-#srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4} --suffix _0.5s --segment_length 0.5
+srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4} --suffix _0.5s --segment_length 0.5
+srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model ${2} --plot True --band_minimum ${3} --band_maximum ${4} --suffix _0.5s --segment_length 0.5
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 0 --model periodogram --plot True --band_minimum 5 --band_maximum 16
 #srun python sliding_window.py --candidates_run True --candidate_id ${1} --n_qpos 1 --model periodogram --plot True --band_minimum 5 --band_maximum 16
 #
--- scripts/super_submit.sh
+++ scripts/super_submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for segment in {0..27}
-do
-  for period in {0..46}
-  do
-    sbatch submit.sh $segment $period gaussian_process 10 40
-  done
-done
+#for segment in {0..27}
+#do
+#  for period in {0..46}
+#  do
+#    sbatch submit.sh $segment $period gaussian_process 10 40
+#  done
+#done
 
 #for i in {0..7}
 #do
@@ -14,11 +14,11 @@ done
 #  sbatch submit.sh ${i} periodogram 5 16
 #done
 #
-#for i in {0..17}
-#do
-#  sbatch submit.sh ${i} gaussian_process 10 40
-#  sbatch submit.sh ${i} periodogram 10 40
-#done
+for i in {0..23}
+do
+  sbatch submit.sh ${i} gaussian_process 40 128
+  sbatch submit.sh ${i} periodogram 40 128
+done
 #
 #for i in {0..11}
 #do
