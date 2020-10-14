000eeeda5646eefef0c87b257d103a07bdcf5a2d
Moritz Huebner <email@moritz-huebner.de>
2020-10-14 16:39:51 +1100

    set up for miller candidate runs

--- scripts/super_submit.sh
+++ scripts/super_submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#for segment in {0..27}
-#do
-#  for period in {0..46}
-#  do
-#    sbatch submit.sh $segment $period gaussian_process 10 40
-#  done
-#done
+for segment in {0..27}
+do
+  for period in {0..46}
+  do
+    sbatch submit.sh $segment $period gaussian_process 10 40
+  done
+done
 
 #for i in {0..7}
 #do
@@ -14,11 +14,11 @@
 #  sbatch submit.sh ${i} periodogram 5 16
 #done
 #
-for i in {0..17}
-do
-  sbatch submit.sh ${i} gaussian_process 10 40
-  sbatch submit.sh ${i} periodogram 10 40
-done
+#for i in {0..17}
+#do
+#  sbatch submit.sh ${i} gaussian_process 10 40
+#  sbatch submit.sh ${i} periodogram 10 40
+#done
 #
 #for i in {0..11}
 #do
