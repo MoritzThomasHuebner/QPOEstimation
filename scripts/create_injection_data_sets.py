6d7adcdaefa2b31fc0c2cddc0371d02afd7cc2f1
Moritz Huebner <email@moritz-huebner.de>
2020-10-29 22:10:27 +1100

    Now draws injections from priors

--- scripts/create_injection_data_sets.py
+++ scripts/create_injection_data_sets.py
@@ -1,20 +1,27 @@
-import numpy as np
-import bilby
-from QPOEstimation.poisson import poisson_process
-from QPOEstimation.model.series import sine_gaussian_with_background
-from QPOEstimation.likelihood import CeleriteLikelihood
-import matplotlib.pyplot as plt
-from pathlib import Path
-import matplotlib
-import bilby
-import stingray
-from copy import deepcopy
+import argparse
 import json
-import celerite
-from QPOEstimation.model.series import PolynomialMeanModel
-from QPOEstimation.likelihood import QPOTerm
+import sys
+from copy import deepcopy
+from pathlib import Path
 
-matplotlib.use('Qt5Agg')
+import bilby
+import matplotlib
+import numpy as np
+
+from QPOEstimation.likelihood import QPOTerm
+from QPOEstimation.model.series import PolynomialMeanModel
+
+if len(sys.argv) > 1:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--minimum_id", default=0, type=int)
+    parser.add_argument("--maximum_id", default=100, type=int)
+    args = parser.parse_args()
+    minimum_id = args.minimum_id
+    maximum_id = args.maximum_id
+else:
+    matplotlib.use('Qt5Agg')
+    minimum_id = 0
+    maximum_id = 100
 
 
 def conversion_function(sample):
@@ -25,15 +32,15 @@ def conversion_function(sample):
 sampling_frequency = 256
 
 priors = bilby.core.prior.PriorDict()
-priors['mean:a0'] = bilby.core.prior.Uniform(minimum=-1000, maximum=1000, name='mean:a0')
-priors['mean:a1'] = bilby.core.prior.Uniform(minimum=-1000, maximum=1000, name='mean:a1')
-priors['mean:a2'] = bilby.core.prior.Uniform(minimum=-1000, maximum=1000, name='mean:a2')
-priors['mean:a3'] = bilby.core.prior.Uniform(minimum=-1000, maximum=1000, name='mean:a3')
-priors['mean:a4'] = bilby.core.prior.Uniform(minimum=-1000, maximum=1000, name='mean:a4')
-priors['kernel:log_a'] = bilby.core.prior.Uniform(minimum=-5, maximum=15, name='log_a')
+priors['mean:a0'] = bilby.core.prior.Uniform(minimum=-10, maximum=10, name='mean:a0')
+priors['mean:a1'] = bilby.core.prior.Uniform(minimum=-10, maximum=10, name='mean:a1')
+priors['mean:a2'] = bilby.core.prior.Uniform(minimum=-10, maximum=10, name='mean:a2')
+priors['mean:a3'] = bilby.core.prior.Uniform(minimum=-10, maximum=10, name='mean:a3')
+priors['mean:a4'] = bilby.core.prior.Uniform(minimum=-10, maximum=10, name='mean:a4')
+priors['kernel:log_a'] = bilby.core.prior.Uniform(minimum=-1, maximum=1, name='log_a')
 priors['kernel:log_b'] = bilby.core.prior.DeltaFunction(peak=-10, name='log_b')
-priors['kernel:log_c'] = bilby.core.prior.Uniform(minimum=-6, maximum=np.log(sampling_frequency), name='log_c')
-priors['kernel:log_f'] = bilby.core.prior.Uniform(minimum=np.log(10), maximum=np.log(64), name='log_f')
+priors['kernel:log_c'] = bilby.core.prior.Uniform(minimum=1, maximum=np.log(sampling_frequency), name='log_c')
+priors['kernel:log_f'] = bilby.core.prior.Uniform(minimum=np.log(5), maximum=np.log(64), name='log_f')
 priors['decay_constraint'] = bilby.core.prior.Constraint(minimum=-1000, maximum=-0.5, name='decay_constraint')
 priors.conversion_function = conversion_function
 
@@ -42,17 +49,13 @@ sampling_frequency = 256
 Path('injection_files/no_qpo').mkdir(exist_ok=True, parents=True)
 Path('injection_files/one_qpo').mkdir(exist_ok=True, parents=True)
 
-for injection_id in range(1000):
-    res_id = np.random.randint(0, 18, 1)[0]
-    res = bilby.result.read_in_result(f'sliding_window_10_40Hz_candidates/one_qpo/results/{res_id}_result.json')
-    params = res.posterior.iloc[np.random.randint(len(res.posterior))]
+for injection_id in range(minimum_id, maximum_id):
+    params = priors.sample()
     params_mean = dict(a0=params['mean:a0'], a1=params['mean:a1'], a2=params['mean:a2'],
                        a3=params['mean:a3'], a4=params['mean:a4'])
     params_kernel = dict(log_a=params['kernel:log_a'], log_b=-10,
                          log_c=params['kernel:log_c'], log_f=params['kernel:log_f'])
-    params = dict(**params)
-    del params['log_likelihood']
-    del params['log_prior']
+
     mean_model = PolynomialMeanModel(**params_mean)
     kernel = QPOTerm(**params_kernel)
 
@@ -64,6 +67,15 @@ for injection_id in range(1000):
     with open(f'injection_files/no_qpo/{str(injection_id).zfill(2)}_params.json', 'w') as f:
         json.dump(params_mean, f)
 
+    # jitter_kernel = celerite.terms.JitterTerm(log_sigma=-20)
+    # gp = celerite.GP(kernel=jitter_kernel, mean=mean_model)
+    # gp.compute(t, yerr)
+    #
+    # x = np.linspace(t[0], t[-1], 5000)
+    # pred_mean_poly, pred_var = gp.predict(y, x, return_var=True)
+    # pred_std = np.sqrt(pred_var)
+
+    K = np.diag(np.ones(len(t)))
     for i in range(len(t)):
         for j in range(len(t)):
             dt = t[1] - t[0]
@@ -73,3 +85,23 @@ for injection_id in range(1000):
     np.savetxt(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_data.txt', np.array([t, y]).T)
     with open(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_params.json', 'w') as f:
         json.dump(params, f)
+
+    # gp = celerite.GP(kernel=kernel, mean=mean_model)
+    # gp.compute(t, yerr)
+    # for param, value in params_random.items():
+    #     gp.set_parameter(param, value)
+    # x = np.linspace(t[0], t[-1], 5000)
+    # pred_mean, pred_var = gp.predict(y, x, return_var=True)
+    # pred_std = np.sqrt(pred_var)
+    #
+    # color = "#ff7f0e"
+    # plt.errorbar(t, y, yerr=np.ones(len(t)), fmt=".k", capsize=0, label='data')
+    # plt.plot(x, pred_mean, color=color, label='Prediction')
+    # plt.fill_between(x, pred_mean + pred_std, pred_mean - pred_std, color=color, alpha=0.3,
+    #                  edgecolor="none")
+    #
+    # plt.plot(x, pred_mean_poly, color='green', label='Polynomial')
+    # plt.legend()
+    # plt.savefig(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_data.pdf')
+    # plt.show()
+    # plt.clf()
