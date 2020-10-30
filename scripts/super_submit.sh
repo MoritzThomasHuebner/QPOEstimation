730420de53963a2339e9d370084ae0189e080b9c
Moritz Huebner <email@moritz-huebner.de>
2020-10-30 20:26:27 +1100

    Set up for more candidate runs

--- scripts/super_submit.sh
+++ scripts/super_submit.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-for segment in {0..27}
+#for segment in {0..27}
+#do
+#  for period in {0..46}
+#  do
+#    sbatch submit.sh $segment $period gaussian_process 64 128
+#  done
+#done
+
+for i in {0..51}
 do
-  for period in {0..46}
-  do
-    sbatch submit.sh $segment $period gaussian_process 64 128
-  done
+  sbatch submit.sh ${i} gaussian_process 5 64
+done
+for i in {0..45}
+do
+  sbatch submit.sh ${i} gaussian_process 64 128
+done
+for i in {0..29}
+do
+  sbatch submit.sh ${i} gaussian_process 128 256
 done
 
-#for i in {0..7}
-#do
-#  sbatch submit.sh ${i} gaussian_process 5 16
-#  sbatch submit.sh ${i} periodogram 5 16
-#done
 #
 #for i in {0..22}
 #do
