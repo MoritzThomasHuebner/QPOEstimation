cad34074b261a167bf76821e6574b815d3cd8efc
Moritz Huebner <email@moritz-huebner.de>
2020-10-31 00:15:47 +1100

    Create more injections

--- scripts/pp_injection_test.py
+++ scripts/pp_injection_test.py
@@ -3,7 +3,7 @@ import json
 
 res_list = []
 for injection_id in range(100):
-    with open(f'{str(injection_id).zfill(2)}_params.json') as f:
+    with open(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_params.json') as f:
         injection_params = json.load(f)
     res = bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/results/{str(injection_id).zfill(2)}_result.json')
     res.injection_parameters = injection_params
