945824719a442399612732b0c220108880215d9c
Moritz Huebner <email@moritz-huebner.de>
2020-10-31 00:09:49 +1100

    Create more injections

--- scripts/pp_injection_test.py
+++ scripts/pp_injection_test.py
@@ -1,5 +1,5 @@
 import bilby
 
-res_list = [bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/{str(injection_id).zfill(2)}_result.json') for injection_id in range(100)]
+res_list = [bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/results/{str(injection_id).zfill(2)}_result.json') for injection_id in range(100)]
 
 bilby.result.make_pp_plot(results=res_list, outdir='.')
