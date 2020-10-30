a6c1cbda5d2ea82f6c4dc37a1f7e1233f185606a
Moritz Huebner <email@moritz-huebner.de>
2020-10-31 00:34:35 +1100

    Reduced number of results

--- scripts/pp_injection_test.py
+++ scripts/pp_injection_test.py
@@ -2,7 +2,7 @@ import bilby
 import json
 
 res_list = []
-for injection_id in range(1000):
+for injection_id in range(0, 100):
     with open(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_params.json') as f:
         injection_params = json.load(f)
     res = bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/results/{str(injection_id).zfill(2)}_result.json')
@@ -12,7 +12,7 @@ for injection_id in range(1000):
 bilby.result.make_pp_plot(results=res_list, filename='pp_test_one_qpo.png')
 
 res_list = []
-for injection_id in range(1000):
+for injection_id in range(0, 100):
     with open(f'injection_files/no_qpo/{str(injection_id).zfill(2)}_params.json') as f:
         injection_params = json.load(f)
     res = bilby.result.read_in_result(f'sliding_window_5_64Hz_no_qpo_injections/no_qpo/results/{str(injection_id).zfill(2)}_result.json')
