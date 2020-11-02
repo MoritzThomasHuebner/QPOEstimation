fdf4103d30fb1c29745e47065110bc774bf2b04a
Moritz Huebner <email@moritz-huebner.de>
2020-11-02 16:26:27 +1100

    Set up for new injection runs

--- scripts/pp_injection_test.py
+++ scripts/pp_injection_test.py
@@ -3,11 +3,14 @@ import json
 
 res_list = []
 for injection_id in range(0, 100):
-    with open(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_params.json') as f:
-        injection_params = json.load(f)
-    res = bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/results/{str(injection_id).zfill(2)}_result.json')
-    res.injection_parameters = injection_params
-    res_list.append(res)
+    try:
+        with open(f'injection_files/one_qpo/{str(injection_id).zfill(2)}_params.json') as f:
+            injection_params = json.load(f)
+        res = bilby.result.read_in_result(f'sliding_window_5_64Hz_one_qpo_injections/one_qpo/results/{str(injection_id).zfill(2)}_result.json')
+        res.injection_parameters = injection_params
+        res_list.append(res)
+    except Exception:
+        continue
 
 bilby.result.make_pp_plot(results=res_list, filename='pp_test_one_qpo.png')
 
