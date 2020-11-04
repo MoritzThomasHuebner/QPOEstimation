229f0745ce7c0c9d047a82d96c76ed0b7772e395
Moritz Huebner <email@moritz-huebner.de>
2020-11-04 20:46:19 +1100

    Rewrote injection script into class

--- QPOEstimation/injection.py
+++ QPOEstimation/injection.py
@@ -10,66 +10,115 @@ from QPOEstimation.likelihood import QPOTerm, ExponentialTerm
 from QPOEstimation.model.series import PolynomialMeanModel
 
 
-def create_injection(params, injection_mode, sampling_frequency, segment_length,
-                     outdir='injection_files', injection_id=0, plot=False):
-    injection_id = str(injection_id).zfill(2)
-    Path(f'{outdir}/{injection_mode}').mkdir(exist_ok=True, parents=True)
+class InjectionCreator(object):
 
-    if isinstance(params, bilby.core.prior.PriorDict):
-        params = params.sample()
-    params_mean = dict()
-    params_kernel = dict()
-    for param, value in params.items():
-        if param.startswith('mean:'):
-            params_mean[param.replace('mean:', '')] = value
-        elif param.startswith('kernel:'):
-            params_kernel[param.replace('kernel:', '')] = value
+    def __init__(self, params, injection_mode, sampling_frequency, segment_length,
+                 outdir='injection_files', injection_id=0):
+        self.params = params
+        self.injection_mode = injection_mode
+        self.sampling_frequency = sampling_frequency
+        self.segment_length = segment_length
+        self.outdir = outdir
+        self.injection_id = str(injection_id).zfill(2)
+        self.create_outdir()
 
-    if injection_mode == "red_noise":
-        kernel = ExponentialTerm(**params_kernel)
-    elif injection_mode == "qpo":
+        self.kernel = self.get_kernel()
+        self.mean_model = PolynomialMeanModel(**self.params_mean)
+
+        self.times = np.linspace(0, self.segment_length, self.sampling_frequency * self.segment_length)
+        self.n = len(self.times)
+        self.dt = self.times[1] - self.times[0]
+
+        self.yerr = np.ones(self.n)
+        self.cov = self.get_cov()
+        self.y = self.get_y()
+
+    def create_outdir(self):
+        Path(f'{self.outdir}/{self.injection_mode}').mkdir(exist_ok=True, parents=True)
+
+    @property
+    def params(self):
+        return self._params
+
+    @params.setter
+    def params(self, params):
+        if isinstance(params, bilby.core.prior.PriorDict):
+            self._params = params.sample()
+        else:
+            self._params = params
+
+    @property
+    def params_mean(self):
+        params_mean = dict()
+        for param, value in self.params.items():
+            if param.startswith('mean:'):
+                params_mean[param.replace('mean:', '')] = value
+        return params_mean
+
+    @property
+    def params_kernel(self):
+        params_kernel = dict()
+        for param, value in self.params.items():
+            if param.startswith('kernel:'):
+                params_kernel[param.replace('kernel:', '')] = value
         params_kernel['log_b'] = -10
-        kernel = QPOTerm(**params_kernel)
+        return params_kernel
 
-    mean_model = PolynomialMeanModel(**params_mean)
-    t = np.linspace(0, segment_length, sampling_frequency * segment_length)
-    dt = t[1] - t[0]
-    yerr = np.ones(len(t))
-    cov = np.diag(np.ones(len(t)))
+    def get_kernel(self):
+        if self.injection_mode == "red_noise":
+            kernel = ExponentialTerm(**self.params_kernel)
+        elif self.injection_mode == "qpo":
+            kernel = QPOTerm(**self.params_kernel)
+        else:
+            kernel = None
+        return kernel
 
-    if injection_mode == "white_noise":
-        y = np.random.multivariate_normal(mean_model.get_value(t), cov)
-        np.savetxt(f'{outdir}/{injection_mode}/{injection_id}_data.txt', np.array([t, y]).T)
-        with open(f'{outdir}/{injection_mode}/{injection_id}_params.json', 'w') as f:
-            json.dump(params_mean, f)
-    else:
-        for i in range(len(t)):
-            for j in range(len(t)):
-                tau = np.abs(i - j) * dt
-                cov[i][j] += kernel.get_value(tau=tau)
-        y = np.random.multivariate_normal(mean_model.get_value(t), cov)
-        np.savetxt(f'{outdir}/{injection_mode}/{injection_id}_data.txt', np.array([t, y]).T)
-        with open(f'{outdir}/{injection_mode}/{injection_id}_params.json', 'w') as f:
-            json.dump(params, f)
+    def get_cov(self):
+        cov = np.diag(np.ones(self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                tau = np.abs(i - j) * self.dt
+                cov[i][j] += self.kernel.get_value(tau=tau)
+        return cov
 
-    if plot:
-        x = np.linspace(t[0], t[-1], 5000)
+    def get_y(self):
+        return np.random.multivariate_normal(self.mean_model.get_value(self.times), self.cov)
+
+    def save(self):
+        np.savetxt(f'{self.outdir}/{self.injection_mode}/{self.injection_id}_data.txt',
+                   np.array([self.times, self.y]).T)
+        with open(f'{self.outdir}/{self.injection_mode}/{self.injection_id}_params.json', 'w') as f:
+            json.dump(self.params, f)
+
+    def plot(self):
+        x = np.linspace(self.times[0], self.times[-1], 5000)
         color = "#ff7f0e"
-        plt.errorbar(t, y, yerr=np.ones(len(t)), fmt=".k", capsize=0, label='data')
-        if injection_mode != 'white_nosie':
-            gp = celerite.GP(kernel=kernel, mean=mean_model)
-            gp.compute(t, yerr)
-            for param, value in params.items():
+        plt.errorbar(self.times, self.y, yerr=np.ones(self.n), fmt=".k", capsize=0, label='data')
+        if self.injection_mode != 'white_nosie':
+            gp = celerite.GP(kernel=self.kernel, mean=self.mean_model)
+            gp.compute(self.times, self.yerr)
+            for param, value in self.params.items():
                 gp.set_parameter(param, value)
-            pred_mean, pred_var = gp.predict(y, x, return_var=True)
+            pred_mean, pred_var = gp.predict(self.y, x, return_var=True)
             pred_std = np.sqrt(pred_var)
             plt.plot(x, pred_mean, color=color, label='Prediction')
             plt.fill_between(x, pred_mean + pred_std, pred_mean - pred_std,
                              color=color, alpha=0.3, edgecolor="none")
 
-        pred_mean_poly = mean_model.get_value(x)
+        pred_mean_poly = self.mean_model.get_value(x)
         plt.plot(x, pred_mean_poly, color='green', label='Mean function')
         plt.legend()
-        plt.savefig(f'{outdir}/{injection_mode}/{injection_id}_data.pdf')
+        plt.savefig(f'{self.outdir}/{self.injection_mode}/{self.injection_id}_data.pdf')
         plt.show()
         plt.clf()
+
+
+def create_injection(params, injection_mode, sampling_frequency, segment_length,
+                     outdir='injection_files', injection_id=0, plot=False):
+    injection_creator = InjectionCreator(params=params, injection_mode=injection_mode,
+                                         sampling_frequency=sampling_frequency, segment_length=segment_length,
+                                         outdir=outdir, injection_id=injection_id)
+
+    injection_creator.save()
+    if plot:
+        injection_creator.plot()
