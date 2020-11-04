1744519bb183bfb97baf0b2b6ff784ce66e2b8c8
Moritz Huebner <email@moritz-huebner.de>
2020-11-04 23:03:19 +1100

    renamed create_injection_data_sets.py to inject.py

--- scripts/submit_create_injections.sh
+++ scripts/submit_create_injections.sh
@@ -6,4 +6,4 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=1G
 
-srun python create_injection_data_sets.py --minimum_id ${1} --maximum_id  ${2} --injection_mode ${3}
+srun python inject.py --minimum_id ${1} --maximum_id  ${2} --injection_mode ${3}
