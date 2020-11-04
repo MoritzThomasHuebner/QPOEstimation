3e5bed52d2f24a1bba45c4fc9585a042d2896785
Moritz Huebner <email@moritz-huebner.de>
2020-11-04 23:34:50 +1100

    Set up for injection runs

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=test
 #
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=15:00
 #SBATCH --mem-per-cpu=1G
 
 srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode qpo --model gaussian_process --plot True --band_minimum 10 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c 1 --polynomial_max 0
