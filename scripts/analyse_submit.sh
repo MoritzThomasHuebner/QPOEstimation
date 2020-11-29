2f2ae82a16b1ccf0c5433dc59d4c399cef5f8532
Moritz Huebner <email@moritz-huebner.de>
2020-11-29 23:52:37 +1100

    set up

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -3,15 +3,16 @@
 #SBATCH --job-name=test
 #
 #SBATCH --ntasks=1
-#SBATCH --time=2:00:00
+#SBATCH --time=30:00
 #SBATCH --mem-per-cpu=1G
 
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode qpo --model gaussian_process --plot True --band_minimum 10 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model mean
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode red_noise --model gaussian_process --plot True --band_minimum 10 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model mean
 
-srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
-srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
-srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode mixed --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
+srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode white_noise --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
+#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
+#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
+#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode mixed --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --data_mode normal --segment_length 0.945 --segment_step 0.23625 --plot False --try_load True --use_ratio True
 
 #srun python analyse.py  --run_mode candidates --candidate_id ${1} --recovery_mode red_noise --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --polynomial_max 1000 --data_mode normal --segment_length 1 --plot True --try_load True --resume True
 #srun python analyse.py  --run_mode candidates --candidate_id ${1} --recovery_mode qpo --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --polynomial_max 1000 --data_mode normal --segment_length 1 --plot True --try_load True --resume True
