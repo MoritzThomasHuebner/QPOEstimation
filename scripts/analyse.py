67a9195c2ab2bb04f5d1890886133673ed4f6ab5
Moritz Huebner <email@moritz-huebner.de>
2021-01-06 14:10:24 +1100

    Changed window priors

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -375,7 +375,7 @@ if likelihood_model in ["gaussian_process", "gaussian_process_windowed"]:
     # gp = celerite.GP(kernel=kernel, mean=mean_model, fit_mean=fit_mean)
     # gp.compute(t, np.sqrt(stabilised_variance))
     if likelihood_model == "gaussian_process_windowed":
-        priors['window_minimum'] = bilby.core.prior.Uniform(minimum=t[0], maximum=t[-1], name='window_minimum')
+        priors['window_minimum'] = bilby.core.prior.Uniform(minimum=t[0], maximum=0.3, name='window_minimum')
         priors['window_size'] = bilby.core.prior.Uniform(minimum=0.3, maximum=0.7, name='window_size')
         priors['window_maximum'] = bilby.core.prior.Constraint(minimum=-1000, maximum=t[-1], name='window_maximum')
 
--- scripts/inject.py
+++ scripts/inject.py
@@ -134,7 +134,7 @@ if likelihood_model == "gaussian_process_windowed":
     # priors['window_minimum'] = bilby.core.prior.Beta(minimum=t[0], maximum=t[-1], alpha=1, beta=2, name='window_minimum')
     # priors['window_maximum'] = MinimumPrior(minimum=t[0], maximum=t[-1], order=1, reference_name='window_minimum', name='window_maximum', minimum_spacing=0.5)
     # priors['window_maximum'] = bilby.core.prior.Uniform(minimum=t[0], maximum=t[-1], name='window_maximum')
-    priors['window_minimum'] = bilby.core.prior.Uniform(minimum=t[0], maximum=t[-1], name='window_minimum')
+    priors['window_minimum'] = bilby.core.prior.Uniform(minimum=t[0], maximum=0.3, name='window_minimum')
     priors['window_size'] = bilby.core.prior.Uniform(minimum=0.3, maximum=0.7, name='window_size')
     priors['window_maximum'] = bilby.core.prior.Constraint(minimum=-1000, maximum=1, name='window_maximum')
 
