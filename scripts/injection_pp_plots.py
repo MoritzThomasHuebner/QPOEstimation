83ab7a1c4ea1193ffa1bd28c8fa3387edc2c4e70
Moritz Huebner <email@moritz-huebner.de>
2021-02-14 21:42:04 +1100

    Improved syntax

--- scripts/injection_pp_plots.py
+++ scripts/injection_pp_plots.py
@@ -4,9 +4,33 @@ import numpy as np
 from copy import deepcopy
 from QPOEstimation.prior.minimum import MinimumPrior
 
+import sys
+import argparse
+
+
+if len(sys.argv) > 1:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--minimum_id", default=0, type=int)
+    parser.add_argument("--maximum_id", default=100, type=int)
+    parser.add_argument("--injection_mode", default="qpo", choices=["qpo", "white_noise", "red_noise"], type=str)
+    parser.add_argument("--likelihood_model", default="gaussian_process",
+                        choices=["gaussian_process", "gaussian_process_windowed"], type=str)
+    parser.add_argument("--polynomial_max", default=10, type=int)
+    args = parser.parse_args()
+    minimum_id = args.minimum_id
+    maximum_id = args.maximum_id
+    injection_mode = args.injection_mode
+    likelihood_model = args.likelihood_model
+    polynomial_max = args.polynomial_max
+else:
+    minimum_id = 0
+    maximum_id = 100
+
+    polynomial_max = 10
+    injection_mode = "qpo"
+    likelihood_model = "gaussian_process"
+
 samples = []
-injection_mode = 'red_noise'
-polynomial_max = 10
 min_log_a = -2
 max_log_a = 1
 min_log_c = -1
@@ -14,31 +38,19 @@ min_log_c = -1
 band_minimum = 5
 band_maximum = 64
 
-segment_length = 1
-sampling_frequency = 256
-t = np.linspace(0, segment_length, int(sampling_frequency * segment_length))
+reslist = []
+for i in range(minimum_id, maximum_id):
+    try:
+        with open(f'injection_files/{injection_mode}/{str(i).zfill(2)}_params.json') as f:
+            injection_params = json.load(f)
+        res = bilby.result.read_in_result(f'injection_{band_minimum}_{band_maximum}Hz_normal_{injection_mode}/{injection_mode}/results/{str(i).zfill(2)}_{likelihood_model}_result.json')
+        reslist.append(res)
+        reslist[-1].injection_parameters = injection_params
+    except (OSError, FileNotFoundError) as e:
+        print(e)
+        continue
 
-# for likelihood_model in ['gaussian_process', 'gaussian_process_windowed']:
-for likelihood_model in ['gaussian_process']:
-    reslist = []
-    for i in range(0, 100):
-        try:
-            with open(f'injection_files/{injection_mode}/{str(i).zfill(2)}_params.json') as f:
-                injection_params = json.load(f)
-
-            if likelihood_model == 'gaussian_process_windowed':
-                res = bilby.result.read_in_result(f'injection_{band_minimum}_{band_maximum}Hz_normal_{injection_mode}/{injection_mode}/results/{str(i).zfill(2)}_gaussian_process_windowed_result.json')
-            else:
-                res = bilby.result.read_in_result(f'injection_{band_minimum}_{band_maximum}Hz_normal_{injection_mode}/{injection_mode}/results/{str(i).zfill(2)}_gaussian_process_result.json')
-
-            reslist.append(res)
-            reslist[-1].injection_parameters = injection_params
-        except (OSError, FileNotFoundError) as e:
-            print(e)
-            continue
-
-
-    if likelihood_model == 'gaussian_process_windowed':
-        bilby.result.make_pp_plot(results=reslist, filename=f'{injection_mode}_pp_plot_windowed_new.png')
-    else:
-        bilby.result.make_pp_plot(results=reslist, filename=f'{injection_mode}_pp_plot_unwindowed_new.png')
+if likelihood_model == 'gaussian_process_windowed':
+    bilby.result.make_pp_plot(results=reslist, filename=f'{injection_mode}_pp_plot_windowed_new.png')
+else:
+    bilby.result.make_pp_plot(results=reslist, filename=f'{injection_mode}_pp_plot_unwindowed_new.png')
