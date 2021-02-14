04aa972628e3f4a35f284dcac8960161635f12e6
Moritz Huebner <email@moritz-huebner.de>
2021-02-14 20:23:17 +1100

    Set up analysis

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -6,13 +6,14 @@
 #SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=1G
 
+srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode red_noise --recovery_mode red_noise --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 100
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --minimum_window_spacing 0.5 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 500
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode qpo --model gaussian_process --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --minimum_window_spacing 0.5 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 500
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode red_noise --model gaussian_process --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 4 --min_log_c -4 --background_model mean
 
 #srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode white_noise  --model gaussian_process --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True
 #srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode ${6}          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 2.0 --segment_step 0.23625 --plot True --try_load False --use_ratio False --nlive 1000
-srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode ${6}          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 2.8 --segment_step 0.945 --plot True --try_load False --use_ratio False
+#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode ${6}          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 2.8 --segment_step 0.945 --plot True --try_load False --use_ratio False
 
 #srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True --use_ratio True
 #srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise    --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True --use_ratio True
--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-for segment in {0..7}
+#for segment in {0..7}
+#do
+#  for period in {0..46}
+#  do
+#    sbatch analyse_submit.sh $segment $period 5 64 gaussian_process_windowed red_noise
+#    sbatch analyse_submit.sh $segment $period 5 64 gaussian_process_windowed zeroed_mixed
+#  done
+#done
+
+for injection in {0..99}
 do
-  for period in {0..46}
-  do
-    sbatch analyse_submit.sh $segment $period 5 64 gaussian_process_windowed red_noise
-    sbatch analyse_submit.sh $segment $period 5 64 gaussian_process_windowed zeroed_mixed
-  done
+  sbatch analyse_submit.sh ${injection} gaussian_process
 done
