2ae08eb902a1bbf3b7ea1a57e2e5e4c93129a231
Moritz Huebner <email@moritz-huebner.de>
2021-02-15 17:45:06 +1100

    Setup to redo runs with more livepoints

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=qpo
 #
 #SBATCH --ntasks=1
-#SBATCH --time=3:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=1G
 
-srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode general_qpo --recovery_mode general_qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 200 --plot False
+srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode general_qpo --model ${3} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 1000 --plot False
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode red_noise --recovery_mode red_noise --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode red_noise --recovery_mode qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
 #srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode red_noise --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
