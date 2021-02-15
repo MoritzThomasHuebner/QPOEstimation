ffeec9ac2b0afe58c1bdd4a5184deb4510f393ee
Moritz Huebner <email@moritz-huebner.de>
2021-02-15 15:36:46 +1100

    Test for why window maximums don't work

--- scripts/plot_window_maximums.py
+++ scripts/plot_window_maximums.py
@@ -14,7 +14,7 @@ for i in range(100, 200):
         with open(f'injection_files/{injection_mode}/{str(i).zfill(2)}_params.json') as f:
             injection_params = json.load(f)
         res = bilby.result.read_in_result(f'injection_{band_minimum}_{band_maximum}Hz_normal_{injection_mode}/{injection_mode}/results/{str(i).zfill(2)}_gaussian_process_windowed_result.json')
-        plt.plot(res.posterior['window_maximum'])
+        plt.hist(res.posterior['window_maximum'], bins='fd')
         plt.axhline(injection_params['window_maximum'])
         plt.savefig(f'{injection_mode}_injections/{str(i).zfill(2)}.png')
         plt.clf()
