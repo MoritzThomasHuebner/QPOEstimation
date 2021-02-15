52cdf3a133db71db13cdc7068e33e85e7dbc77a5
Moritz Huebner <email@moritz-huebner.de>
2021-02-15 12:02:48 +1100

    set up for evaluation

--- scripts/evalutate_identical_injections.py
+++ scripts/evalutate_identical_injections.py
@@ -6,31 +6,42 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-band_minimum = 5
-band_maximum = 64
 
-likelihood_model = 'gaussian_process'
+if len(sys.argv) > 1:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--minimum_id", default=1000, type=int)
+    parser.add_argument("--maximum_id", default=2000, type=int)
+    parser.add_argument("--injection_mode", default="qpo", choices=["qpo", "white_noise", "red_noise"], type=str)
+    parser.add_argument("--likelihood_model", default="gaussian_process",
+                        choices=["gaussian_process", "gaussian_process_windowed"], type=str)
+    parser.add_argument("--band_minimum", default=5, type=float)
+    parser.add_argument("--band_maximum", default=64, type=float)
+
+    args = parser.parse_args()
+    minimum_id = args.minimum_id
+    maximum_id = args.maximum_id
+    injection_mode = args.injection_mode
+    likelihood_model = args.likelihood_model
+    band_minimum = args.band_minimum
+    band_maximum = args.band_maximum
+else:
+    matplotlib.use('Qt5Agg')
+    minimum_id = 1000
+    maximum_id = 2000
+
+    band_minimum = 5
+    band_maximum = 64
+
+    injection_mode = "qpo"
+    likelihood_model = "gaussian_process"
+
 
 band = f'{band_minimum}_{band_maximum}Hz'
 
-log_bfs_one_qpo_gpr = []
-log_bfs_one_qpo_whittle = []
-
-
 # averaged runs
 
-log_a = 1
-log_c = -1
-log_f = np.log(20)
-
-log_evidences_qpo = []
-log_evidences_red_noise = []
-averaged_log_bfs_qpo_v_red_noise = []
-averaged_log_bfs_qpo_v_red_noise_err = []
-
-injection_mode = "red_noise"
 log_bfs = []
-for injection_id in range(1000, 1100):
+for injection_id in range(minimum_id, maximum_id):
     res_qpo = bilby.result.read_in_result(f"injection_{band}_normal_{injection_mode}/qpo/results/{injection_id}_{likelihood_model}_result.json")
     res_red_noise = bilby.result.read_in_result(f"injection_{band}_normal_{injection_mode}/red_noise/results/{injection_id}_{likelihood_model}_result.json")
     log_bfs.append(res_qpo.log_evidence - res_red_noise.log_evidence)
@@ -40,7 +51,7 @@ for injection_id in range(1000, 1100):
 min_ln_bf = np.min(log_bfs)
 max_ln_bf = np.max(log_bfs)
 
-plt.hist(log_bfs, bins=50, label="100 injections")
+plt.hist(log_bfs, bins="fd", label="1000 injections")
 plt.axvline(min_ln_bf, label=f"minimum ln BF {min_ln_bf:.2f}", color='orange')
 plt.axvline(max_ln_bf, label=f"maximum ln BF {max_ln_bf:.2f}", color='green')
 plt.xlabel("ln BF QPO")
