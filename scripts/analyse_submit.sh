f292ab36bf104acf2745c5380f7523174fd1c2d7
Moritz Huebner <email@moritz-huebner.de>
2021-02-15 13:32:29 +1100

    Increased wall time

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=qpo
 #
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem-per-cpu=1G
 
 srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode general_qpo --recovery_mode general_qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 200 --plot False
