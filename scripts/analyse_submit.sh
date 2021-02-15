00aae48bab18347545950d31a3ae36d5a9385ec3
Moritz Huebner <email@moritz-huebner.de>
2021-02-15 17:51:10 +1100

    Set up for new preliminary runs

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -3,36 +3,7 @@
 #SBATCH --job-name=qpo
 #
 #SBATCH --ntasks=1
-#SBATCH --time=24:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem-per-cpu=1G
 
-srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode general_qpo --model ${3} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 1000 --plot False
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode red_noise --recovery_mode red_noise --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode red_noise --recovery_mode qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode red_noise --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 200 --plot False
-
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode qpo --model ${2} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --minimum_window_spacing 0.5 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 500
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode qpo --recovery_mode qpo --model gaussian_process --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --minimum_window_spacing 0.5 --background_model polynomial --polynomial_max 10 --segment_length 1 --nlive 500
-#srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode red_noise --model gaussian_process --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 4 --min_log_c -4 --background_model mean
-
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode white_noise  --model gaussian_process --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode ${6}          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 2.0 --segment_step 0.23625 --plot True --try_load False --use_ratio False --nlive 1000
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode ${6}          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 2.8 --segment_step 0.945 --plot True --try_load False --use_ratio False
-
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo          --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True --use_ratio True
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise    --model ${5}             --band_minimum ${3} --band_maximum ${4} --background_model polynomial --data_mode normal --segment_length 1.8 --segment_step 0.23625 --plot True --try_load True --use_ratio True
-
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo          --model gaussian_process_windowed --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode blind_injection --segment_length 2.0 --segment_step 0.23625 --plot False
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise    --model gaussian_process_windowed --band_minimum ${3} --band_maximum ${4} --background_model mean --data_mode smoothed_residual --segment_length 1.8 --segment_step 0.23625 --plot True
-#
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode white_noise --model gaussian_process --band_minimum 64 --band_maximum 128 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo --model gaussian_process_windowed --band_minimum 64 --band_maximum 128 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise --model gaussian_process_windowed --band_minimum 64 --band_maximum 128 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-#
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode white_noise --model gaussian_process --band_minimum 128 --band_maximum 256 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode qpo --model gaussian_process_windowed --band_minimum 128 --band_maximum 256 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-#srun python analyse.py  --run_id ${1} --period_number ${2} --recovery_mode red_noise --model gaussian_process_windowed --band_minimum 128 --band_maximum 256 --background_model mean --data_mode smoothed_residual --segment_length 2 --segment_step 0.23625 --plot False --try_load True --use_ratio False
-
-#srun python analyse.py  --run_mode candidates --candidate_id ${1} --recovery_mode red_noise --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --polynomial_max 1000 --data_mode normal --segment_length 1 --plot True --try_load True --resume True
-#srun python analyse.py  --run_mode candidates --candidate_id ${1} --recovery_mode qpo --model gaussian_process --band_minimum 5 --band_maximum 64 --background_model polynomial --polynomial_max 1000 --data_mode normal --segment_length 1 --plot True --try_load True --resume True
+srun python analyse.py --run_mode injection --injection_id ${1} --injection_mode ${2} --recovery_mode general_qpo --model ${3} --plot True --band_minimum 5 --band_maximum 64 --min_log_a -2 --max_log_a 1 --min_log_c -1 --background_model polynomial --polynomial_max 0 --segment_length 1 --nlive 1000 --plot False
--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -10,14 +10,14 @@
 
 for injection in {0..99}
 do
-  sbatch analyse_submit.sh ${injection} qpo gaussian_process
+#  sbatch analyse_submit.sh ${injection} qpo gaussian_process
   sbatch analyse_submit.sh ${injection} red_noise gaussian_process
-  sbatch analyse_submit.sh ${injection} general_qpo gaussian_process
+#  sbatch analyse_submit.sh ${injection} general_qpo gaussian_process
 done
 
 for injection in {100..199}
 do
-  sbatch analyse_submit.sh ${injection} qpo gaussian_process_windowed
+#  sbatch analyse_submit.sh ${injection} qpo gaussian_process_windowed
   sbatch analyse_submit.sh ${injection} red_noise gaussian_process_windowed
-  sbatch analyse_submit.sh ${injection} general_qpo gaussian_process_windowed
+#  sbatch analyse_submit.sh ${injection} general_qpo gaussian_process_windowed
 done
