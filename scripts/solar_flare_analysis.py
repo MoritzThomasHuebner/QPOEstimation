31f0f842115703935cdc0c302b625e42061ff8e8
Moritz Huebner <email@moritz-huebner.de>
2021-02-22 22:03:06 +1100

    Collective changes

--- scripts/solar_flare_analysis.py
+++ scripts/solar_flare_analysis.py
@@ -38,9 +38,9 @@ matplotlib.use('Qt5Agg')
 
 minimum_window_spacing = 0
 
-recovery_mode = "qpo"
-likelihood_model = "gaussian_process"
-background_model = "fred"
+recovery_mode = "pure_qpo"
+likelihood_model = "gaussian_process_windowed"
+background_model = "gaussian"
 
 
 nlive = 300
@@ -52,8 +52,8 @@ suffix = ""
 
 
 data = np.loadtxt(f'data/SolarFlare/120704187_ctime_lc.txt')
+# outdir = f"SolarFlare/120704187/"
 outdir = f"SolarFlare/120704187/"
-# outdir = f"SolarFlare/120704187_segments_end/"
 
 # data = np.loadtxt(f'solar_flare_injection/qpo/1_fred_injection_data.txt')
 # import json
@@ -69,8 +69,6 @@ outdir = f"SolarFlare/120704187/"
 # outdir = f"SolarFlare/injection/"
 
 
-
-
 times = data[:, 0]
 counts = data[:, 1]
 try:
@@ -83,38 +81,27 @@ except Exception:
 # max_rate = np.amax(counts)
 # counts /= max_rate
 
-times = times[60:440]
-counts = counts[60:440]
-yerr = yerr[60:440]
+# times = times[60:250]
+# counts = counts[60:250]
+# yerr = yerr[60:250]
+# times = times[155:345]
+# counts = counts[155:345]
+# yerr = yerr[155:345]
+# times = times[250:440]
+# counts = counts[250:440]
+# yerr = yerr[250:440]
+times = times[60:]
+counts = counts[60:]
+yerr = yerr[60:]
+# times = times[60:440]
+# counts = counts[60:440]
+# yerr = yerr[60:440]
 
+plt.plot(times, counts)
+plt.show()
 # yerr /= max_rate
 
-def boxcar_filter(cs, n):
-    res = np.zeros(len(cs))
-    for i, c in enumerate(cs):
-        if i < (n - 1)/2:
-            boxcar = cs[0: int(i + (n - 1) / 2)]
-        elif i > len(cs) - (n - 1)/2:
-            boxcar = cs[int(i - (n - 1) / 2):-1]
-        else:
-            boxcar = cs[int(i - (n - 1) / 2): int(i + (n - 1) / 2)]
-        res[i] = sum(boxcar) / len(boxcar)
 
-    return res
-
-# from scipy.signal._savitzky_golay import savgol_filter
-# window_length = 151
-# filtered_counts = savgol_filter(counts, window_length, 3)
-# filtered_counts = boxcar_filter(counts, 21)
-# plt.plot(times, counts)
-# plt.plot(times, filtered_counts)
-# plt.show()
-
-# plt.errorbar(times, counts - filtered_counts, yerr=yerr, fmt=".k", capsize=0, label='data')
-# plt.show()
-
-# counts -= filtered_counts
-# label = f"savgol_filtered_{recovery_mode}_windowed_{window_length}"
 
 
 start = times[0]
@@ -128,59 +115,45 @@ band_minimum = 1/(stop - start)
 band_maximum = sampling_frequency/2
 band = f'{band_minimum}_{band_maximum}Hz'
 
-priors = bilby.core.prior.PriorDict()
+priors = bilby.core.prior.ConditionalPriorDict()
 
-# kernel = get_kernel(kernel_type=recovery_mode)
-# kernel_priors = get_kernel_prior(kernel_type=recovery_mode, min_log_a=-15, max_log_a=10.,
-#                                  min_log_c=-15, band_minimum=band_minimum, band_maximum=band_maximum)
-# priors.update(kernel_priors)
-kernel = celerite.terms.SHOTerm(log_S0=0, log_Q=np.log(1 / np.sqrt(2)), log_omega0=1)
-n_shos = 3
 
-for i in range(1, n_shos):
-    kernel += celerite.terms.SHOTerm(log_S0=0, log_Q=np.log(1/np.sqrt(2)), log_omega0=1)
 
-# priors['kernel:terms[0]:log_a'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_a')
-# priors['kernel:terms[0]:log_b'] = bilby.core.prior.DeltaFunction(peak=-20, name='log_b')
-# priors['kernel:terms[0]:log_c'] = bilby.core.prior.Uniform(minimum=-15, maximum=np.log(band_maximum), name='log_c')
-# priors['kernel:terms[0]:log_f'] = bilby.core.prior.Uniform(minimum=np.log(band_minimum), maximum=np.log(band_maximum), name='log_f')
+# kernel = get_kernel(kernel_type=recovery_mode) + get_kernel(recovery_mode) + get_kernel(recovery_mode)
+# priors['kernel:terms[0]:log_a'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='kernel:terms[0]:log_a')
+# priors['kernel:terms[0]:log_c'] = bilby.core.prior.Uniform(minimum=-15, maximum=np.log(band_maximum), name='kernel:terms[0]:log_c')
+# priors['kernel:terms[1]:log_a'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='kernel:terms[1]:log_a')
+# priors['kernel:terms[1]:log_c'] = bilby.core.prior.Uniform(minimum=-15, maximum=np.log(band_maximum), name='kernel:terms[1]:log_c')
+# priors['kernel:terms[2]:log_a'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='kernel:terms[2]:log_a')
+# priors['kernel:terms[2]:log_c'] = bilby.core.prior.Uniform(minimum=-15, maximum=np.log(band_maximum), name='kernel:terms[2]:log_c')
+#
+# priors['kernel:terms[0]:log_f'] = Beta(minimum=np.log(band_minimum), maximum=np.log(band_maximum), alpha=1, beta=3, name='kernel:terms[0]:log_f')
+# priors['kernel:terms[1]:log_f'] = MinimumPrior(minimum=np.log(band_minimum), maximum=np.log(band_maximum), order=2, minimum_spacing=0, reference_name='kernel:terms[0]:log_f', name='kernel:terms[1]:log_f')
+# priors['kernel:terms[2]:log_f'] = MinimumPrior(minimum=np.log(band_minimum), maximum=np.log(band_maximum), order=1, minimum_spacing=0, reference_name='kernel:terms[1]:log_f', name='kernel:terms[2]:log_f')
+#
 # priors['decay_constraint'] = bilby.core.prior.Constraint(minimum=-1000, maximum=0.0, name='decay_constraint')
+# priors._resolve_conditions()
 
-# priors['kernel:terms[1]:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_S0_0')
-# priors['kernel:terms[1]:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1/np.sqrt(2)), name='log_a_0')
-# priors['kernel:terms[1]:log_omega0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_omega0_0')
-#
-# priors['kernel:terms[2]:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_S0_1')
-# priors['kernel:terms[2]:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1/np.sqrt(2)), name='log_a_1')
-# priors['kernel:terms[2]:log_omega0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_omega0_1')
-#
-# priors['kernel:terms[2]:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_S0_2')
-# priors['kernel:terms[2]:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1/np.sqrt(2)), name='log_a_2')
-# priors['kernel:terms[2]:log_omega0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name='log_omega0_2')
 
-minimum_spacing = 0
-for ii in range(n_shos):
-    initial_term = 0
-    if ii == 0:
-        priors[f'kernel:terms[{ii+initial_term}]:log_S0'] = Beta(minimum=-15, maximum=30, alpha=1, beta=n_shos, name=f'log_S0_{ii}')
-    else:
-        priors[f'kernel:terms[{ii+initial_term}]:log_S0'] = QPOEstimation.prior.minimum.MinimumPrior(
-            order=n_shos-ii, minimum_spacing=minimum_spacing, minimum=-15, maximum=30, name=f'log_S0_{ii}')
-    # priors[f'kernel:terms[{ii+initial_term}]:log_S0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name=f'log_S0_{ii}')
-    # priors[f'kernel:terms[{ii+initial_term}]:log_Q'] = bilby.core.prior.DeltaFunction(peak=np.log(1 / np.sqrt(2)), name=f'log_a_{ii}')
-    priors[f'kernel:terms[{ii+initial_term}]:log_Q'] = bilby.core.prior.Uniform(minimum=np.log(1 / np.sqrt(2)), maximum=30, name=f'log_a_{ii}')
-    priors[f'kernel:terms[{ii+initial_term}]:log_omega0'] = bilby.core.prior.Uniform(minimum=-15, maximum=30, name=f'log_omega0_{ii}')
 
-# label = 'sho_red_noise_kernel'
+min_log_c = np.log(1/(times[-1] - times[0]))
+kernel = get_kernel(kernel_type=recovery_mode)
+kernel_priors = get_kernel_prior(kernel_type=recovery_mode, min_log_a=-15, max_log_a=30.,
+                                 min_log_c=min_log_c, band_minimum=band_minimum, band_maximum=band_maximum)
+priors.update(kernel_priors)
+
+# priors.conversion_function = decay_constrain_conversion_function
+
 # mean_model = np.mean(counts)
 # kernel = celerite.terms.JitterTerm(log_sigma=-20)
 # priors["kernel:log_sigma"] = bilby.core.prior.DeltaFunction(peak=-20, name='log_sigma')
 
 
-n_models = 1
-# label = f'gaussian_{n_models}'
+n_models = 5
+label = f'gaussian_{n_models}'
 
 
+# mean_model = 0
 mean_model = get_n_component_mean_model(model=gaussian, n_models=n_models)
 
 mean_priors = ConditionalPriorDict()
@@ -195,15 +168,32 @@ if n_models == 1:
 else:
     for ii in range(n_models):
         if ii == 0:
-            mean_priors[f"mean:t_0_{ii}"] = Beta(minimum=t_min, maximum=t_max, alpha=1, beta=n_models, name=f"t_0_{ii}")
+            mean_priors[f"mean:t_0_{ii}"] = Beta(minimum=t_min, maximum=t_max, alpha=1, beta=n_models, name=f"mean:t_0_{ii}")
         else:
             mean_priors[f"mean:t_0_{ii}"] = QPOEstimation.prior.minimum.MinimumPrior(
-                order=n_models - ii, minimum_spacing=minimum_spacing, minimum=t_min, maximum=t_max, name=f"t_0_{ii}")
+                order=n_models - ii, minimum_spacing=minimum_spacing, minimum=t_min, maximum=t_max, name=f"mean:t_0_{ii}")
         mean_priors[f'mean:amplitude_{ii}'] = bilby.core.prior.LogUniform(minimum=1e-3, maximum=1e12, name=f'A_{ii}')
         mean_priors[f'mean:sigma_{ii}'] = bilby.core.prior.LogUniform(minimum=1e-3, maximum=10000, name=f'sigma_{ii}')
         mean_priors[f"mean:t_0_{ii}"].__class__.__name__ = "QPOEstimation.prior.minimum.MinimumPrior"
 
 priors.update(mean_priors)
+
+
+# n_freds = 1
+# label = f"fred_{n_freds}_{recovery_mode}"
+# label = f"fred_{n_freds}_{recovery_mode}"
+
+# mean_model = get_n_component_fred_model(n_freds=n_freds)
+
+# minimum_spacing = 0
+# t_min = 0
+# t_max = 2000
+#
+# mean_priors = get_fred_priors(n_freds=n_freds, t_min=t_min, t_max=t_max, minimum_spacing=minimum_spacing)
+# priors.update(mean_priors)
+
+priors._resolve_conditions()
+
 
 # label = f'stabilised_gaussian_{n_models}'
 # label = f'stabilised_gaussian_gp_{n_models}_{recovery_mode}'
@@ -213,13 +203,12 @@ priors.update(mean_priors)
 
 # mean_model = get_n_component_fred_model(n_freds=n_freds)
 # mean_model = get_n_component_stabilised_fred_model(n_freds=n_freds)
-# mean_priors = get_fred_priors(n_freds=n_freds, t_min=0, t_max=2000, minimum_spacing=0)
 # priors.update(mean_priors)
 # priors['kernel:log_f'].minimum -= 0.2
 
 
 # mean_model = np.mean(counts)
-label = f'{n_models}_gaussian_{n_shos}_shos_only'
+# label = f'{n_models}_gaussian_{n_shos}_shos_only'
 if likelihood_model == 'gaussian_process':
     likelihood = CeleriteLikelihood(kernel=kernel, mean_model=mean_model, fit_mean=True, t=times,
                                     y=counts, yerr=yerr)
@@ -229,6 +218,8 @@ else:
     window_priors = get_window_priors(times=times)
     priors.update(window_priors)
 
+
+priors._resolve_conditions()
 # plt.plot(times, mean_model.get_value(times))
 # plt.errorbar(times, counts, yerr=yerr, fmt=".k", capsize=0, label='data')
 # plt.show()
@@ -249,8 +240,9 @@ try:
     result.plot_corner(outdir=f"{outdir}/corner", truths=truths)
 except Exception:
     result.plot_corner(outdir=f"{outdir}/corner")
-# max_like_params = result.posterior.iloc[-1]
-# plt.plot(times, counts)
+max_like_params = result.posterior.iloc[-1]
+
+
 # plt.plot(times, gauss(times, **max_like_params), color='red')
 # for i in range(10):
 #     params = result.posterior.iloc[np.random.randint(len(result.posterior))]
@@ -265,6 +257,8 @@ if plot:
                 frequency_samples = np.exp(np.array(result.posterior['kernel:log_f']))
             except Exception as e:
                 frequency_samples = np.exp(np.array(result.posterior['kernel:terms[0]:log_f']))
+                np.savetxt(f"{outdir}/corner/freq_samples_0.txt", frequency_samples)
+
             plt.hist(frequency_samples, bins="fd", density=True)
             plt.xlabel('frequency [Hz]')
             plt.ylabel('normalised PDF')
@@ -274,6 +268,38 @@ if plot:
                 f"{np.mean(frequency_samples):.2f} + {percentiles[1] - median:.2f} / - {- percentiles[0] + median:.2f}")
             plt.savefig(f"{outdir}/corner/{label}_frequency_posterior")
             plt.clf()
+
+            try:
+                frequency_samples = np.exp(np.array(result.posterior['kernel:terms[1]:log_f']))
+                np.savetxt(f"{outdir}/corner/freq_samples_1.txt", frequency_samples)
+                plt.hist(frequency_samples, bins="fd", density=True)
+                plt.xlabel('frequency [Hz]')
+                plt.ylabel('normalised PDF')
+                median = np.median(frequency_samples)
+                percentiles = np.percentile(frequency_samples, [16, 84])
+                plt.title(
+                    f"{np.mean(frequency_samples):.2f} + {percentiles[1] - median:.2f} / - {- percentiles[0] + median:.2f}")
+                plt.savefig(f"{outdir}/corner/{label}_frequency_posterior_1")
+                plt.clf()
+            except Exception as e:
+                pass
+
+            try:
+                frequency_samples = np.exp(np.array(result.posterior['kernel:terms[2]:log_f']))
+                np.savetxt(f"{outdir}/corner/freq_samples_2.txt", frequency_samples)
+                plt.hist(frequency_samples, bins="fd", density=True)
+                plt.xlabel('frequency [Hz]')
+                plt.ylabel('normalised PDF')
+                median = np.median(frequency_samples)
+                percentiles = np.percentile(frequency_samples, [16, 84])
+                plt.title(
+                    f"{np.mean(frequency_samples):.2f} + {percentiles[1] - median:.2f} / - {- percentiles[0] + median:.2f}")
+                plt.savefig(f"{outdir}/corner/{label}_frequency_posterior_2")
+                plt.clf()
+            except Exception as e:
+                pass
+
+
         except Exception as e:
             bilby.core.utils.logger.info(e)
 
@@ -298,11 +324,8 @@ if plot:
 
     if likelihood_model == 'gaussian_process_windowed':
         plt.axvline(max_like_params['window_minimum'], color='cyan', label='start/end stochastic process')
-        # plt.axvline(max_like_params['window_minimum'] + max_like_params['window_size'], color='cyan')
         plt.axvline(max_like_params['window_maximum'], color='cyan')
-        # x = np.linspace(max_like_params['window_minimum'], max_like_params['window_minimum'] + max_like_params['window_size'], 5000)
         x = np.linspace(max_like_params['window_minimum'], max_like_params['window_maximum'], 5000)
-        # windowed_indices = np.where(np.logical_and(max_like_params['window_minimum'] < t, t < max_like_params['window_minimum'] + max_like_params['window_size']))
         windowed_indices = np.where(
             np.logical_and(max_like_params['window_minimum'] < times, times < max_like_params['window_maximum']))
         likelihood.gp.compute(times[windowed_indices], yerr[windowed_indices])
@@ -314,7 +337,8 @@ if plot:
         pred_std = np.sqrt(pred_var)
 
     color = "#ff7f0e"
-    plt.errorbar(times, counts, yerr=yerr, fmt=".k", capsize=0, label='data')
+    # plt.errorbar(times, counts, yerr=yerr, fmt=".k", capsize=0, label='data')
+    plt.plot(times, counts, label='data')
     plt.plot(x, pred_mean, color=color, label='Prediction')
     plt.fill_between(x, pred_mean + pred_std, pred_mean - pred_std, color=color, alpha=0.3,
                      edgecolor="none")
@@ -332,9 +356,12 @@ if plot:
         plt.plot(x, trend, color='green', label='Mean')
 
     plt.xlabel("time [s]")
-    plt.ylabel("data")
+    # plt.ylabel("data")
+    plt.ylabel("photon counts")
     plt.legend()
+    plt.tight_layout()
     plt.savefig(f"{outdir}/fits/{label}_max_like_fit")
+
     plt.show()
     plt.clf()
 
