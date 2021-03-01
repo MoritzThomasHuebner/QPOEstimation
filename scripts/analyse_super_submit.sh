177f6463da32b17ef097835c4e48e5e1ec8b2f29
MoritzThomasHuebner <email@moritz-huebner.de>
2021-03-01 18:49:50 +1100

    Restart red noise runs

--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -5,9 +5,9 @@ do
   for period in {13..14}
   do
     sbatch analyse_submit.sh giant_flare ${period} ${segment} red_noise  gaussian_process
-    sbatch analyse_submit.sh giant_flare ${period} ${segment} general_qpo  gaussian_process
+#    sbatch analyse_submit.sh giant_flare ${period} ${segment} general_qpo  gaussian_process
     sbatch analyse_submit.sh giant_flare ${period} ${segment} red_noise  gaussian_process_windowed
-    sbatch analyse_submit.sh giant_flare ${period} ${segment} general_qpo  gaussian_process_windowed
+#    sbatch analyse_submit.sh giant_flare ${period} ${segment} general_qpo  gaussian_process_windowed
   done
 done
 
