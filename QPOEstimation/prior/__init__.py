733afe5c3f124c6bf920404e186c5929aa90e0a9
MoritzThomasHuebner <email@moritz-huebner.de>
2021-03-01 18:49:32 +1100

    Now correctly calculate the segment length

--- QPOEstimation/prior/__init__.py
+++ QPOEstimation/prior/__init__.py
@@ -8,9 +8,10 @@ from . import minimum, psd, gp, mean
 def get_priors(**kwargs):
     """ Catch all function"""
 
+    segment_length = kwargs['times'][-1] - kwargs['times'][0]
     if kwargs['min_log_c'] is None:
         if kwargs['kernel_type'] == 'red_noise':
-            kwargs['min_log_c'] = np.log(1 / kwargs['segment_length'])
+            kwargs['min_log_c'] = np.log(1 / segment_length)
         else:
             kwargs['min_log_c'] = -10
     if kwargs['max_log_c'] is None:
