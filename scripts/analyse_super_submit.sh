63a120e46e2be77862f810d208d6c195db5b70cc
MoritzThomasHuebner <email@moritz-huebner.de>
2021-03-01 16:20:07 +1100

    Set up for some test runs on giant flare

--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#for segment in {0..7}
-#do
-#  for period in {0..46}
-#  do
-#    sbatch analyse_submit.sh $segment $period 5 64 gaussian_process_windowed red_noise
-#  done
-#done
-
-for injection_id in {0..99}
+for segment in {0..7}
 do
-  sbatch analyse_submit.sh injection ${injection_id} gaussian_process
-  sbatch analyse_submit.sh injection ${injection_id} gaussian_process_windowed
+  for period in {13..14}
+  do
+    sbatch analyse_submit.sh giant_flare ${period} ${segment} qpo  gaussian_process
+  done
 done
 
+#for injection_id in {0..99}
+#do
+#  sbatch analyse_submit.sh injection ${injection_id} gaussian_process
+#  sbatch analyse_submit.sh injection ${injection_id} gaussian_process_windowed
+#done
+#
