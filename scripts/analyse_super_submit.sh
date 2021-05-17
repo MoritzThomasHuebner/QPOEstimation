3772f98a5ee61c6ba83d584f8ea1c915e1d33f6a
Moritz Huebner <email@moritz-huebner.de>
2021-05-17 15:57:55 +1000

    New runs

--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -26,7 +26,8 @@ for n_components in {0..4}
 do
    for model in red_noise general_qpo
    do
-     sbatch analyse_submit.sh ${model} ${n_components}
+     sbatch analyse_submit.sh ${model} ${n_components} fred
+     sbatch analyse_submit.sh ${model} ${n_components} skew_gaussian
    done
 done
 
