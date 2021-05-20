57569cedbba001eccc691c262d1b005c876adeb5
MoritzThomasHuebner <email@moritz-huebner.de>
2021-05-20 14:37:35 +1000

    new proper runs

--- scripts/analyse_hh_frequentist.py
+++ scripts/analyse_hh_frequentist.py
@@ -8,8 +8,10 @@ import pandas as pd
 
 import os
 
-
-flares = np.array(sorted(os.listdir('hares_and_hounds_HH2_just_figures')))
+try:
+    flares = np.array(sorted(os.listdir('hares_and_hounds_HH2_just_figures')))
+except Exception:
+    flares = np.array(sorted(os.listdir('hares_and_hounds_HH2')))
 print(len(flares))
 run_mode = 'from_maximum'
 
