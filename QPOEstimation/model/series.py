ffa7436c9161a19bcd98451b5c437c411646b112
Moritz Huebner <email@moritz-huebner.de>
2021-06-01 18:01:25 +1000

    Harmonised function definitions

--- QPOEstimation/model/series.py
+++ QPOEstimation/model/series.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def polynomial(t, a0, a1, a2, a3, a4):
-    times = t.copy()
-    times -= times[0]
-    times -= times[-1] / 2
-    return a0 + a1 * times + a2 * times**2 + a3 * times**3 + a4 * times**4
+def polynomial(times, a0, a1, a2, a3, a4):
+    t = times.copy()
+    t -= t[0]
+    t -= t[-1] / 2
+    return a0 + a1 * t + a2 * t**2 + a3 * t**3 + a4 * t**4
 
 
 def fred(times, amplitude, t_0, sigma_rise, sigma_fall):
@@ -35,26 +35,26 @@ def fred_norris_extended(times, amplitude, log_psi, t_0, delta, log_gamma, log_n
         return amplitude * np.exp(-psi**gamma * frac**gamma - psi**nu /frac**nu) * np.exp(2 * psi)
 
 
-def exponential_background(times, amplitude, tau, offset, **kwargs):
+def exponential_background(times, amplitude, tau, offset):
     return amplitude * np.exp(times/tau) + offset
 
 
-def gaussian(t, amplitude, t_0, sigma):
-    return amplitude * np.exp(-(t - t_0) ** 2 / (2 * sigma ** 2))
+def gaussian(times, amplitude, t_0, sigma):
+    return amplitude * np.exp(-(times - t_0) ** 2 / (2 * sigma ** 2))
 
 
 def skew_gaussian(times, amplitude, t_0, sigma_rise, sigma_fall):
     before_burst_indices = np.where(times <= t_0)
     after_burst_indices = np.where(times > t_0)
     envelope = np.zeros(len(times))
-    envelope[before_burst_indices] = gaussian(t=times[before_burst_indices], amplitude=amplitude, t_0=t_0, sigma=sigma_rise)
-    envelope[after_burst_indices] = gaussian(t=times[after_burst_indices], amplitude=amplitude, t_0=t_0, sigma=sigma_fall)
+    envelope[before_burst_indices] = gaussian(times=times[before_burst_indices], amplitude=amplitude, t_0=t_0, sigma=sigma_rise)
+    envelope[after_burst_indices] = gaussian(times=times[after_burst_indices], amplitude=amplitude, t_0=t_0, sigma=sigma_fall)
     return envelope
 
 
-def log_normal(t, amplitude, t_0, sigma):
-    return amplitude / t * np.exp(-(np.log(t) - t_0)**2 / (2 * sigma**2))
+def log_normal(times, amplitude, t_0, sigma):
+    return amplitude / times * np.exp(-(np.log(times) - t_0) ** 2 / (2 * sigma ** 2))
 
 
-def lorentzian(t, amplitude, t_0, sigma):
-    return amplitude * (sigma / ((t - t_0) ** 2 + (sigma ** 2)))
+def lorentzian(times, amplitude, t_0, sigma):
+    return amplitude * (sigma / ((times - t_0) ** 2 + (sigma ** 2)))
