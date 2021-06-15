11f21433a1ae7d5c84f171830a49cd44160405b1
Moritz Huebner <email@moritz-huebner.de>
2021-06-15 11:54:37 +1000

    Try re-run red noise

--- scripts/analyse_periodogram_submit.sh
+++ scripts/analyse_periodogram_submit.sh
@@ -4,7 +4,7 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=8G
 
 
 srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_source injection --injection_id ${4} --injection_mode general_qpo --recovery_mode ${3} --run_mode select_time --injection_file_dir injection_files_pop --injection_likelihood_model gaussian_process_windowed --likelihood_model whittle --sample rwalk --nlive 500 --try_load True --resume True
--- scripts/analyse_periodogram_super_submit.sh
+++ scripts/analyse_periodogram_super_submit.sh
@@ -5,11 +5,8 @@ start_times=(3500 3600 3700 3800 3900 4000 4100 4200 4250 4300 4400 4500 4600 47
 end_times=(6500 6400 6300 6200 6100 6000 5900 5800 5750 5700 5600 5500 5400 5300 5200 5150 5100 5080 5060 5040 5020)
 
 
-for recovery_mode in general_qpo red_noise
+for i in {0..40}
 do
-  for i in {0..40}
-  do
-    sbatch analyse_periodogram_submit.sh ${start_times[$i]} ${end_times[$i]} ${recovery_mode} 0
-    sbatch analyse_periodogram_submit.sh ${start_times[$i]} ${end_times[$i]} ${recovery_mode} 1
-  done
+  sbatch analyse_periodogram_submit.sh ${start_times[$i]} ${end_times[$i]} red_noise 0
+  sbatch analyse_periodogram_submit.sh ${start_times[$i]} ${end_times[$i]} red_noise 1
 done
