757bebe161f37947c40622861623e5515e654113
Moritz Huebner <email@moritz-huebner.de>
2021-06-17 23:55:23 +1000

    More runs

--- scripts/analyse_periodogram_super_submit.sh
+++ scripts/analyse_periodogram_super_submit.sh
@@ -4,7 +4,7 @@
 
 times=($(seq 20 20 2000))
 
-for i in {0..9}
+for i in {10..100}
 do
 #  sbatch analyse_periodogram_submit.sh ${times[$i]} 0 red_noise
 #  sbatch analyse_periodogram_submit.sh ${times[$i]} 1 red_noise
