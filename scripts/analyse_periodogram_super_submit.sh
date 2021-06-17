657a6ab77409736e23b1a49ac5a5aacc32d07ce0
Moritz Huebner <email@moritz-huebner.de>
2021-06-17 18:41:00 +1000

    More runs

--- scripts/analyse_periodogram_super_submit.sh
+++ scripts/analyse_periodogram_super_submit.sh
@@ -4,10 +4,14 @@
 
 times=($(seq 20 20 2000))
 
-for i in {10..101}
+for i in {0..9}
 do
-  sbatch analyse_periodogram_submit.sh ${times[$i]} 0 red_noise
-  sbatch analyse_periodogram_submit.sh ${times[$i]} 1 red_noise
-  sbatch analyse_periodogram_submit.sh ${times[$i]} 0 general_qpo
-  sbatch analyse_periodogram_submit.sh ${times[$i]} 1 general_qpo
+#  sbatch analyse_periodogram_submit.sh ${times[$i]} 0 red_noise
+#  sbatch analyse_periodogram_submit.sh ${times[$i]} 1 red_noise
+  sbatch analyse_periodogram_submit.sh ${times[$i]} 2 red_noise
+  sbatch analyse_periodogram_submit.sh ${times[$i]} 3 red_noise
+#  sbatch analyse_periodogram_submit.sh ${times[$i]} 0 general_qpo
+#  sbatch analyse_periodogram_submit.sh ${times[$i]} 1 general_qpo
+  sbatch analyse_periodogram_submit.sh ${times[$i]} 2 general_qpo
+  sbatch analyse_periodogram_submit.sh ${times[$i]} 3 general_qpo
 done
