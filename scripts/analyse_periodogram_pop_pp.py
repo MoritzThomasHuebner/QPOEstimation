b5ee8d9d9f83cffd26feddbf58b0fe6bcceb35e4
Moritz Huebner <email@moritz-huebner.de>
2021-06-17 18:38:20 +1000

    More runs

--- scripts/analyse_periodogram_pop_pp.py
+++ scripts/analyse_periodogram_pop_pp.py
@@ -12,6 +12,7 @@ from QPOEstimation.prior.gp import *
 from pathlib import Path
 import sys
 import json
+import warnings
 
 
 matplotlib.use('Qt5Agg')
@@ -20,7 +21,7 @@ modes = ['zeros', 'white_noise']
 mode = 0
 
 
-end_times = np.arange(20, 200, 20)
+end_times = np.arange(20, 2020, 20)
 start_times = -end_times
 durations = 2 * end_times
 
@@ -66,11 +67,10 @@ print(x_break)
 
 ln_bfs = []
 log_frequency_spreads = []
-ln_bfs_zero_padded = []
 durations_reduced = []
 snr_optimals = []
-snr_matched_filters = []
 extension_factors = []
+delta_bics = []
 
 for start_time, end_time, duration in zip(start_times, end_times, durations):
     extension_factor = duration/durations[0]
@@ -82,11 +82,11 @@ for start_time, end_time, duration in zip(start_times, end_times, durations):
     label_red_noise = f'{injection_id}_{float(start_time)}_{float(end_time)}'
     label_qpo = f'{injection_id}_{float(start_time)}_{float(end_time)}'
     try:
-        res_red_noise_periodogram = bilby.result.read_in_result(outdir=outdir_red_noise_periodogram, label=label_red_noise)
-        res_qpo_periodogram = bilby.result.read_in_result(outdir=outdir_qpo_periodogram, label=label_qpo)
-        ln_bfs.append(res_qpo_periodogram.log_evidence - res_red_noise_periodogram.log_evidence)
-        log_frequency_spreads.append(np.std(res_qpo_periodogram.posterior['log_frequency']))
-        # plt.hist(np.exp(res_qpo_periodogram.posterior['log_frequency']), histtype='step', label=f"{duration}s",
+        res_noise = bilby.result.read_in_result(outdir=outdir_red_noise_periodogram, label=label_red_noise)
+        res_qpo = bilby.result.read_in_result(outdir=outdir_qpo_periodogram, label=label_qpo)
+        ln_bfs.append(res_qpo.log_evidence - res_noise.log_evidence)
+        log_frequency_spreads.append(np.std(res_qpo.posterior['log_frequency']))
+        # plt.hist(np.exp(res_qpo.posterior['log_frequency']), histtype='step', label=f"{duration}s",
         #          density=True, bins='fd')
         durations_reduced.append(duration)
         extension_factors.append(extension_factor)
@@ -115,20 +115,25 @@ for start_time, end_time, duration in zip(start_times, end_times, durations):
         frequency_array=frequencies, psd_array=psd_array_qpo_diluted)
     psd_signal_diluted = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
         frequency_array=frequencies, psd_array=psd_array_signal_diluted)
-    snr_qpo_optimal = np.sqrt(np.sum(
-        np.nan_to_num((psd_qpo_diluted.power_spectral_density_interpolated(freqs_combined_periodogram) /
-                       psd_noise_diluted.power_spectral_density_interpolated(freqs_combined_periodogram)) ** 2, nan=0)))
-    print(snr_qpo_optimal)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        snr_qpo_optimal = np.sqrt(np.sum(
+            np.nan_to_num((psd_qpo_diluted.power_spectral_density_interpolated(freqs_combined_periodogram) /
+                           psd_noise_diluted.power_spectral_density_interpolated(freqs_combined_periodogram)) ** 2, nan=0)))
+    # print(snr_qpo_optimal)
     snr_optimals.append(snr_qpo_optimal)
 
     ### Matched filter SNR calculation, doesnt make sense yet
-    max_like_params = res_qpo_periodogram.posterior.iloc[-1]
-    alpha_max_like = max_like_params['alpha']
-    beta_max_like = np.exp(max_like_params['log_beta'])
-    white_noise_max_like = np.exp(max_like_params['log_sigma'])
-    amplitude_max_like = np.exp(max_like_params['log_amplitude'])
-    width_max_like = np.exp(max_like_params['log_width'])
-    central_frequency_max_like = np.exp(max_like_params['log_frequency'])
+    bic_qpo = 6 * np.log(len(y_selected)) - 2 * res_qpo.maximum_log_likelihood
+    bic_noise = 3 * np.log(len(y_selected)) - 2 * res_noise.maximum_log_likelihood
+    delta_bics.append(bic_qpo - bic_noise)
+
+    alpha_max_like = res_qpo.maximum_likelihood_parameters['alpha']
+    beta_max_like = np.exp(res_qpo.maximum_likelihood_parameters['log_beta'])
+    white_noise_max_like = np.exp(res_qpo.maximum_likelihood_parameters['log_sigma'])
+    amplitude_max_like = np.exp(res_qpo.maximum_likelihood_parameters['log_amplitude'])
+    width_max_like = np.exp(res_qpo.maximum_likelihood_parameters['log_width'])
+    central_frequency_max_like = np.exp(res_qpo.maximum_likelihood_parameters['log_frequency'])
     # psd_array_noise_max_like = QPOEstimation.model.psd.red_noise(frequencies=freqs_combined_periodogram, alpha=alpha_max_like, beta=beta_max_like) + white_noise_max_like
     # psd_array_qpo_max_like = QPOEstimation.model.psd.lorentzian(
     #     frequencies=freqs_combined_periodogram, amplitude=amplitude_max_like, width=width_max_like,
@@ -146,30 +151,45 @@ for start_time, end_time, duration in zip(start_times, end_times, durations):
     # plt.loglog(freqs_combined_periodogram, psd_array_noise_max_like)
     # plt.show()
 
+    dofs = len(freqs_combined_periodogram) - 6
+
     ### Chi-squared tests
     chi_square = QPOEstimation.model.psd.periodogram_chi_square_test(
         frequencies=freqs_combined_periodogram, powers=powers_combined_periodogram,
-        psd=psd_signal_diluted, degrees_of_freedom=6)
+        psd=psd_signal_diluted, degrees_of_freedom=dofs)
     print(chi_square)
 
     idxs = QPOEstimation.utils.get_indices_by_time(freqs_combined_periodogram,
                                                    minimum_time=central_frequency_max_like-width_max_like,
                                                    maximum_time=central_frequency_max_like+width_max_like)
+    # idxs = QPOEstimation.utils.get_indices_by_time(freqs_combined_periodogram,
+    #                                                minimum_time=0,
+    #                                                maximum_time=0.8)
+
+    dofs = len(idxs) - 6
+    print(dofs)
     chi_square = QPOEstimation.model.psd.periodogram_chi_square_test(
         frequencies=freqs_combined_periodogram[idxs], powers=powers_combined_periodogram[idxs],
-        psd=psd_signal_diluted, degrees_of_freedom=6)
+        psd=psd_signal_diluted, degrees_of_freedom=dofs)
     print(chi_square)
-    assert False
+    print()
+    # assert False
 
 
+
+ln_bfs = np.array(ln_bfs)
+log_frequency_spreads = np.array(log_frequency_spreads)
+durations_reduced = np.array(durations_reduced)
+snr_optimals = np.array(snr_optimals)
+extension_factors = np.array(extension_factors)
+delta_bics = np.array(delta_bics)
 
 
 np.savetxt('periodogram_pop/temp_ln_bfs', ln_bfs)
 np.savetxt('periodogram_pop/temp_log_frequency_spreads', log_frequency_spreads)
-np.savetxt('periodogram_pop/temp_ln_bfs_zero_padded', ln_bfs_zero_padded)
 np.savetxt('periodogram_pop/temp_durations_reduced', durations_reduced)
 np.savetxt('periodogram_pop/temp_snr_optimals', snr_optimals)
-np.savetxt('periodogram_pop/temp_snr_matched_filters', snr_matched_filters)
+# np.savetxt('periodogram_pop/temp_snr_matched_filters', snr_matched_filters)
 np.savetxt('periodogram_pop/temp_extension_factors', extension_factors)
 
 outdir = "periodogram_pop"
@@ -177,44 +197,44 @@ Path(outdir).mkdir(parents=True, exist_ok=True)
 
 plt.xlabel('f [Hz]')
 plt.ylabel('p(f)')
-plt.xlim(0.2, 1.2)
+# plt.xlim(0.2, 1.2)
 plt.legend()
-# plt.savefig(f'{outdir}/whittle_{modes[mode]}_frequency_histograms.png')
+plt.savefig(f'{outdir}/whittle_{modes[mode]}_frequency_histograms.png')
 plt.show()
 
 plt.plot(extension_factors, snr_optimals, label='Optimal SNR')
-plt.plot(extension_factors, snr_matched_filters, label='Matched Filter SNR')
+# plt.plot(extension_factors, snr_matched_filters, label='Matched Filter SNR')
 plt.xlabel(r'Extension factor $x$')
 plt.ylabel(r'SNR')
 plt.legend()
 plt.savefig(f'periodogram_pop/snr_vs_extension_{modes[mode]}.pdf')
 plt.show()
 
-assert False
 
-ln_bfs_zero_padded.reverse()
-ln_bfs.reverse()
-durations_reduced.reverse()
-log_frequency_spreads_zero_padded.reverse()
-log_frequency_spreads.reverse()
 
-prop_ln_bfs = ln_bfs_zero_padded[0]/durations_reduced[0] * np.array(durations_reduced)
-plt.plot(durations_reduced, ln_bfs, color='red', label="white noise padded")
-plt.plot(durations_reduced, ln_bfs_zero_padded, color='orange', label='zero padded')
-plt.plot(durations_reduced, prop_ln_bfs, color='brown', label='proportional')
-plt.xlabel("segment length [s]")
+prop_ln_bfs = ln_bfs[0] * np.array(extension_factors)
+plt.plot(extension_factors, ln_bfs, color='red')
+plt.plot(extension_factors, prop_ln_bfs, color='blue', label='proportional')
+plt.xlabel("extension factor")
 plt.ylabel("ln BF")
-plt.axvline(durations[0], label="burst length", color='blue', linestyle='-.')
 plt.legend()
-plt.savefig(f'{outdir}/{injection_id}_duration_ln_bf.png')
+plt.savefig(f'{outdir}/{modes[mode]}_duration_ln_bf.png')
 plt.show()
 
 
-plt.plot(durations_reduced, log_frequency_spreads, color='red', label='white noise padded')
-plt.plot(durations_reduced, log_frequency_spreads_zero_padded, color='orange', label='zero padded')
-plt.xlabel("segment length [s]")
-plt.ylabel("Standard deviation of $\ln f$")
-plt.axvline(burst_length, label="burst_length", color='blue', linestyle='-.')
+predicted_extended_delta_bics = delta_bics[0] * extension_factors + (3 - 6) * np.log(extension_factors)
+plt.plot(extension_factors, delta_bics, color='red', label='Inferred $\Delta BIC$')
+plt.plot(extension_factors, predicted_extended_delta_bics, color='blue', label='Predicted $\Delta BIC$')
+plt.xlabel("extension factor")
+plt.ylabel("$\Delta BIC$")
 plt.legend()
-plt.savefig(f'{outdir}/{injection_id}_duration_ln_f_spread.png')
+plt.savefig(f'{outdir}/{modes[mode]}_duration_delta_bic.png')
+plt.show()
+
+
+plt.plot(extension_factors, log_frequency_spreads, color='red')
+plt.xlabel("extension factor")
+plt.ylabel("Standard deviation of $\ln f$")
+# plt.legend()
+plt.savefig(f'{outdir}/{modes[mode]}_duration_ln_f_spread.png')
 plt.show()
