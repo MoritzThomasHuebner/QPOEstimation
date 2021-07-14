2a616dce6193958ae18c0e17aef762fa758bb751
Moritz Huebner <email@moritz-huebner.de>
2021-07-14 14:01:44 +1000

    Fixed priors

--- QPOEstimation/prior/psd.py
+++ QPOEstimation/prior/psd.py
@@ -6,7 +6,7 @@ def get_red_noise_prior():
     prior = bilby.core.prior.ConditionalPriorDict()
     prior['alpha'] = bilby.core.prior.Uniform(0, 20, name='alpha')
     prior['log_beta'] = bilby.core.prior.Uniform(-100, 100, name='log_beta')
-    prior['log_sigma'] = bilby.core.prior.Uniform(-30, 0, name='log_sigma')
+    prior['log_sigma'] = bilby.core.prior.Uniform(-30, 30, name='log_sigma')
     return prior
 
 
@@ -26,7 +26,7 @@ def get_qpo_prior(frequencies=None, **kwargs):
         df = frequencies[1] - frequencies[0]
         max_frequency = frequencies[-1]
     prior = bilby.core.prior.ConditionalPriorDict()
-    prior['log_amplitude'] = bilby.core.prior.Uniform(-30, 0, name='log_amplitude')
+    prior['log_amplitude'] = bilby.core.prior.Uniform(-30, 30, name='log_amplitude')
     prior['log_width'] = bilby.core.prior.ConditionalUniform(condition_func=log_width_condition_func,
         minimum=np.log(df/np.pi), maximum=kwargs.get('max_log_width', np.log(0.25*max_frequency)), name='log_width')
     # prior['log_frequency'] = bilby.core.prior.Uniform(np.log(2*df), np.log(max_frequency), name='log_frequency')
