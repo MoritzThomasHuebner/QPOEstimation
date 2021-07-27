d423e3d79acb802f53e6006ae8f3f970956f6f84
Moritz Huebner <email@moritz-huebner.de>
2021-07-27 15:09:45 +1000

    More runs

--- scripts/analyse_periodogram_super_submit.sh
+++ scripts/analyse_periodogram_super_submit.sh
@@ -55,23 +55,32 @@
 #  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 01 red_noise hann 0.1
 #done
 
-extensions=($(seq 0 5 90))
+#extensions=($(seq 0 5 90))
+#start_time=-10
+#end_time=10
+#
+#
+#for i in {0..18}
+#do
+#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 02 red_noise hann 0.1
+#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 02 general_qpo hann 0.1
+#done
+
+extensions=($(seq 0 10 190))
 start_time=-10
 end_time=10
 
+sbatch analyse_periodogram_submit.sh $((start_time - extensions[0])) $((end_time + extensions[0])) 03 pure_qpo tukey 0.1
+sbatch analyse_periodogram_submit.sh $((start_time - extensions[0])) $((end_time + extensions[0])) 03 white_noise tukey 0.1
+sbatch analyse_periodogram_submit.sh $((start_time - extensions[0])) $((end_time + extensions[0])) 04 pure_qpo tukey 0.1
+sbatch analyse_periodogram_submit.sh $((start_time - extensions[0])) $((end_time + extensions[0])) 04 white_noise tukey 0.1
 
-for i in {0..18}
+for i in {1..19}
 do
-  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 02 red_noise hann 0.1
-  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 02 general_qpo hann 0.1
-done
-
-#for i in {0..19}
-#do
-#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 09 pure_qpo hann 0.025
-#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 09 white_noise hann 0.025
-#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 10 pure_qpo hann 0.025
-#  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 10 white_noise hann 0.025
+  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 03 pure_qpo hann 0.1
+  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 03 white_noise hann 0.1
+  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 04 pure_qpo hann 0.1
+  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 04 white_noise hann 0.1
 #  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 11 white_noise tukey 0.5
 #  sbatch analyse_periodogram_submit.sh $((start_time - extensions[$i])) $((end_time + extensions[$i])) 11 pure_qpo tukey 0.5
-#done
+done
