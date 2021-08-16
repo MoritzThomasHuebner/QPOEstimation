3197da84e1305a4d5fa64fd31b21cad0400bb2fe
Moritz Huebner <email@moritz-huebner.de>
2021-08-16 21:29:30 +1000

    Improved default plots

--- QPOEstimation/plotting.py
+++ QPOEstimation/plotting.py
@@ -8,9 +8,9 @@ def plot_chi_squares(outdir, label, extension_factors, chi_squares=None, chi_squ
         plt.plot(extension_factors, chi_squares, label='Entire spectrum')
     if chi_squares_qpo is not None:
         plt.plot(extension_factors, chi_squares_qpo, label='$f_0 \pm 2\sigma$')
-    if chi_squares_red_noise is not None:
-        if not np.isnan(chi_squares_red_noise[0]):
-            plt.plot(extension_factors, chi_squares_red_noise, label='$f \leq f_{\mathrm{break}, x}$')
+    # if chi_squares_red_noise is not None:
+    #     if not np.isnan(chi_squares_red_noise[0]):
+    #         plt.plot(extension_factors, chi_squares_red_noise, label='$f \leq f_{\mathrm{break}, x}$')
     if chi_squares_high_freqs is not None:
         plt.plot(extension_factors, chi_squares_high_freqs, label="$f \geq f_0 + 2\sigma$")
     plt.xlabel('$x$')
@@ -19,7 +19,7 @@ def plot_chi_squares(outdir, label, extension_factors, chi_squares=None, chi_squ
         np.array([chi_squares, chi_squares_qpo, chi_squares_red_noise, chi_squares_high_freqs]), nan=0))
     top = min(1.5, top) + 0.1
     print(top)
-    plt.ylim(0, top=top)
+    # plt.ylim(0, top=top)
     plt.legend(ncol=2)
     plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1.05),
                fancybox=True, shadow=False, ncol=2)
@@ -67,30 +67,30 @@ def plot_snrs_and_ln_bfs(outdir, label, extension_factors, ln_bfs, snrs_optimal=
                          snrs_max_like_quantiles=None, x_break=None, show=False):
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
     fig, ax = plt.subplots()
-    lines = []
     # if len(snrs_optimal) != 0:
     #     lines.append(ax.plot(extension_factors, snrs_optimal, label='Optimal SNR'))
     # if snrs_max_like is not None:
-    max_like_line = ax.plot(extension_factors, snrs_max_like, color=colors[0], label='Maximum likelihood SNR')
-    lines.append(max_like_line)
+    ax.plot(extension_factors, snrs_max_like, color=colors[0])
+
     if len(snrs_max_like_quantiles) != 0:
         ax.fill_between(extension_factors, snrs_max_like_quantiles[:, 0], snrs_max_like_quantiles[:, 1], color=colors[0], alpha=0.3)
     if x_break is not None:
         ax.axvline(x_break, color='black', linestyle='-.', label='$x_{\mathrm{break}}$')
+        ax.legend()
+    # if True:
+    #     ax.axvline(3, color='black', linestyle='-.', label='$x=3$')
+    #     ax.legend()
     ax.set_xlabel('$x$')
     ax.set_ylabel('SNR', color=colors[0])
+    ax.tick_params(axis='y', labelcolor=colors[0])
+
     ax2 = ax.twinx()
     ax2.set_ylabel("$\ln BF$", color=colors[1])
-    lines.append(ax2.plot(extension_factors, ln_bfs, color=colors[1], label='$\ln BF$'))
+    ax2.tick_params(axis='y', labelcolor=colors[1])
+    ax2.plot(extension_factors, ln_bfs, color=colors[1])
 
-    combined_lines = lines[0]
-    for i, line in enumerate(lines):
-        if i == 0:
-            continue
-        combined_lines += line
-
-    # ax.legend(combined_lines, [line.get_label() for line in combined_lines])
     fig.tight_layout()
+    plt.xlim(extension_factors[0], extension_factors[-1])
     fig.savefig(f'{outdir}/{label}_snrs_and_ln_bf_vs_extension.pdf')
     if show:
         fig.show()
