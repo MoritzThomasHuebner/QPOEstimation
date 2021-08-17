55f5300d4b17c16bcb003b407300a29a5ad293c5
Moritz Huebner <email@moritz-huebner.de>
2021-08-17 17:48:11 +1000

    Changed injections

--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -55,7 +55,7 @@ for injection_mode in red_noise general_qpo
 do
     for recovery_mode in red_noise general_qpo
     do
-        for injection_id in {0..1}
+        for injection_id in {0..99}
         do
           sbatch analyse_submit.sh $injection_id $injection_mode $recovery_mode
         done
