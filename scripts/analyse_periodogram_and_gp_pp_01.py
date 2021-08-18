84f8dc8ba6ead7bf081cae2a06b60f61a693bc97
Moritz Huebner <email@moritz-huebner.de>
2021-08-18 16:18:38 +1000

    Added post-processing scripts

---
+++ scripts/analyse_periodogram_and_gp_pp_01.py
@@ -0,0 +1,122 @@
+import json
+import sys
+
+import numpy as np
+from pathlib import Path
+
+import bilby
+
+import QPOEstimation
+from QPOEstimation.post_processing import InjectionStudyPostProcessor
+
+import matplotlib.pyplot as plt
+# plt.style.use('paper.mplstyle')
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+
+modes = ['zeros', 'white_noise']
+# mode = int(sys.argv[1])
+mode = 1
+injection_id = str(mode).zfill(2)
+outdir = "periodogram_pop"
+Path(outdir).mkdir(parents=True, exist_ok=True)
+normalisation = False
+
+load = True
+n_snrs = 2000
+
+end_times = np.arange(10, 210, 10)
+print(end_times)
+
+start_times = -end_times
+durations = 2 * end_times
+
+
+outdir_qpo_periodogram = f'injection/general_qpo_injection/general_qpo_recovery/whittle/results/'
+outdir_noise_periodogram = f'injection/general_qpo_injection/red_noise_recovery/whittle/results/'
+
+
+data = np.loadtxt(f'injection_files_pop/general_qpo/whittle/{injection_id}_data.txt')
+times = data[:, 0]
+y = data[:, 1]
+sampling_frequency = int(round(1/(times[1] - times[0])))
+with open(f'injection_files_pop/general_qpo/whittle/{injection_id}_params.json', 'r') as f:
+    injection_parameters = json.load(f)
+
+frequencies = np.linspace(1/100000, 20, 1000)
+alpha = injection_parameters['alpha']
+beta = injection_parameters['beta']
+white_noise = injection_parameters['sigma']
+amplitude = injection_parameters['amplitude']
+width = injection_parameters['width']
+central_frequency = injection_parameters['central_frequency']
+
+psd_array_noise = QPOEstimation.model.psd.red_noise(frequencies=frequencies, alpha=alpha, beta=beta) + white_noise
+psd_array_white_noise = white_noise * np.ones(len(frequencies))
+
+psd_noise = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+    frequency_array=frequencies, psd_array=psd_array_noise)
+psd_white_noise = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+    frequency_array=frequencies, psd_array=psd_array_white_noise)
+psd_array_qpo = QPOEstimation.model.psd.lorentzian(frequencies=frequencies, amplitude=amplitude, width=width,
+                                                   central_frequency=central_frequency)
+psd_qpo = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+    frequency_array=frequencies, psd_array=psd_array_qpo)
+
+
+injection_psds = dict(red_noise=psd_noise, qpo=psd_qpo)
+extension_mode = modes[mode]
+
+props = InjectionStudyPostProcessor(
+    start_times=start_times, end_times=end_times, durations=durations, outdir=outdir,
+    label=injection_id, times=times, frequencies=frequencies, normalisation=normalisation, y=y,
+    outdir_noise_periodogram=outdir_noise_periodogram, outdir_qpo_periodogram=outdir_qpo_periodogram,
+    injection_parameters=injection_parameters, injection_psds=injection_psds, extension_mode=extension_mode)
+
+if load:
+    props.ln_bfs = np.loadtxt(f"{outdir}/cached_results/{injection_id}_ln_bfs.txt")
+    props.log_frequency_spreads = np.loadtxt(f"{outdir}/cached_results/{injection_id}_log_frequency_spreads.txt")
+    props.durations_reduced = np.loadtxt(f"{outdir}/cached_results/{injection_id}_durations_reduced.txt")
+    props.snrs_optimal = np.loadtxt(f"{outdir}/cached_results/{injection_id}_snrs_optimal.txt")
+    props.snrs_max_like = np.loadtxt(f"{outdir}/cached_results/{injection_id}_snrs_max_like.txt")
+    props.snrs_max_like_quantiles = np.loadtxt(f"{outdir}/cached_results/{injection_id}_snrs_max_like_quantiles.txt")
+    props.extension_factors = np.loadtxt(f"{outdir}/cached_results/{injection_id}_extension_factors.txt")
+    props.delta_bics = np.loadtxt(f"{outdir}/cached_results/{injection_id}_delta_bics.txt")
+    props.chi_squares = np.loadtxt(f"{outdir}/cached_results/{injection_id}_chi_squares.txt")
+    props.chi_squares_qpo = np.loadtxt(f"{outdir}/cached_results/{injection_id}_chi_squares_qpo.txt")
+    props.chi_squares_red_noise = np.loadtxt(f"{outdir}/cached_results/{injection_id}_chi_squares_red_noise.txt")
+    props.chi_squares_high_freqs = np.loadtxt(f"{outdir}/cached_results/{injection_id}_chi_squares_high_freqs.txt")
+else:
+    props.fill(n_snrs=n_snrs)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_ln_bfs.txt", props.ln_bfs)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_log_frequency_spreads.txt", props.log_frequency_spreads)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_durations_reduced.txt", props.durations_reduced)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_snrs_optimal.txt", props.snrs_optimal)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_snrs_max_like.txt", props.snrs_max_like)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_snrs_max_like_quantiles.txt", props.snrs_max_like_quantiles)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_extension_factors.txt", props.extension_factors)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_delta_bics.txt", props.delta_bics)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_chi_squares.txt", props.chi_squares)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_chi_squares_qpo.txt", props.chi_squares_qpo)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_chi_squares_red_noise.txt", props.chi_squares_red_noise)
+    np.savetxt(f"{outdir}/cached_results/{injection_id}_chi_squares_high_freqs.txt", props.chi_squares_high_freqs)
+
+ln_bfs_gp = []
+ln_bfs_gp_windowed = []
+for end_time in range(10, 210, 10):
+    label = f'{injection_id}_{-float(end_time)}_{float(end_time)}_1_0s'
+    res_qpo = bilby.result.read_in_result(outdir='injection/general_qpo_injection/general_qpo_recovery/gaussian_process/results/', label=label)
+    res_red_noise = bilby.result.read_in_result(outdir='injection/general_qpo_injection/red_noise_recovery/gaussian_process/results/', label=label)
+    res_qpo_windowed = bilby.result.read_in_result(outdir='injection/general_qpo_injection/general_qpo_recovery/gaussian_process_windowed/results/', label=label)
+    res_red_noise_windowed = bilby.result.read_in_result(outdir='injection/general_qpo_injection/red_noise_recovery/gaussian_process_windowed/results/', label=label)
+
+    ln_bfs_gp.append(res_qpo.log_evidence - res_red_noise.log_evidence)
+    ln_bfs_gp_windowed.append(res_qpo_windowed.log_evidence - res_red_noise_windowed.log_evidence)
+
+plt.plot(props.durations, props.ln_bfs)
+plt.plot(props.durations, ln_bfs_gp)
+plt.plot(props.durations, ln_bfs_gp_windowed)
+plt.show()
+
+# props.plot_all(show=False)
---
+++ scripts/analyse_periodogram_pop_goes_pp.py
@@ -0,0 +1,150 @@
+import os
+from pathlib import Path
+
+import bilby.core.series
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.signal import periodogram
+
+import QPOEstimation
+from QPOEstimation.prior.gp import *
+from pathlib import Path
+import sys
+import json
+import warnings
+from QPOEstimation.get_data import get_data
+
+
+matplotlib.use('Qt5Agg')
+
+
+
+outdir_qpo_periodogram = f'solar_flare_go1520130512/select_time/general_qpo/whittle/results/'
+outdir_red_noise_periodogram = f'solar_flare_go1520130512/select_time/red_noise/whittle/results/'
+outdir_bpl_periodogram = f'solar_flare_go1520130512/select_time/broken_power_law/whittle/results/'
+
+outdirs = [outdir_red_noise_periodogram, outdir_qpo_periodogram, outdir_bpl_periodogram]
+# outdirs = [outdir_qpo_periodogram]
+ranges = ['73020_74700', '74700.0_74900.0', '74900_75780', '73020_75780']
+
+for r in ranges:
+    ref_log_z = 0
+    ref_log_z_err = 0
+    ref_bic = 0
+    times, y, _, outdir, label = get_data(
+        data_source='solar_flare', run_mode='select_time', start_time=float(r.split('_')[0]), end_time=float(r.split('_')[1]),
+        likelihood_model='whittle', solar_flare_folder='goes', solar_flare_id="go1520130512")
+    y = (y - np.mean(y)) / np.mean(y)
+    print(r)
+    for outdir in outdirs:
+        res = bilby.result.read_in_result(outdir=outdir, label=r)
+        max_like_params = res.posterior.iloc[-1]
+        if outdir == outdir_qpo_periodogram:
+            k = 6
+        elif outdir == outdir_red_noise_periodogram:
+            k = 3
+        else:
+            k = 5
+
+        if outdir == outdir_red_noise_periodogram:
+            ref_log_z = res.log_evidence
+            ref_log_z_err = res.log_evidence_err
+            ref_bic = k * np.log(len(times)/2) - 2 * max_like_params['log_likelihood']
+
+        print('ln BF vs red noise')
+        print(f"{res.log_evidence - ref_log_z} +/- {np.sqrt(res.log_evidence_err**2 + ref_log_z_err**2)}")
+        print('Delta BIC vs red noise')
+        bic = k * np.log(len(times) / 2) - 2 * max_like_params['log_likelihood']
+        print(f"{bic - ref_bic}")
+        print()
+        # continue
+        if outdir == outdir_qpo_periodogram:
+            samples = np.exp(-res.posterior['log_frequency'])
+            samples = np.array(samples)
+
+            if r == ranges[0]:
+                plot_samples = samples[np.where(samples < 50)]
+                # plt.hist(plot_samples, bins='fd')
+                # plt.show()
+                samples_low = samples[np.where(samples < 10)]
+                samples_high = samples[np.where(np.logical_and(samples >15, samples < 30))]
+                print(np.median(samples_low))
+                print(np.quantile(samples_low, [0.16, 0.84]) - np.median(samples_low))
+                print(np.median(samples_high))
+                print(np.quantile(samples_high, [0.16, 0.84]) - np.median(samples_high))
+            else:
+                # plt.hist(samples, bins='fd')
+                # plt.show()
+                print(np.median(samples))
+                print(np.quantile(samples, [0.16, 0.84]) - np.median(samples))
+
+            sampling_frequency = 1 / (times[1] - times[0])
+            # plt.plot(times, y)
+            # plt.show()
+
+            freqs, powers = periodogram(y, fs=sampling_frequency, window="hann")
+
+            frequencies = np.linspace(1 / 100000, 2, 1000000)
+            alpha = max_like_params['alpha']
+            beta = np.exp(max_like_params['log_beta'])
+            white_noise = np.exp(max_like_params['log_sigma'])
+            amplitude = np.exp(max_like_params['log_amplitude'])
+            width = np.exp(max_like_params['log_width'])
+            central_frequency = np.exp(max_like_params['log_frequency'])
+
+            psd_array_noise = QPOEstimation.model.psd.red_noise(frequencies=frequencies, alpha=alpha,
+                                                                beta=beta) + white_noise
+            psd_array_qpo = QPOEstimation.model.psd.lorentzian(frequencies=frequencies, amplitude=amplitude,
+                                                               width=width, central_frequency=central_frequency)
+            psd_array_white_noise = white_noise * np.ones(len(frequencies))
+
+            psd_noise = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+                frequency_array=frequencies, psd_array=psd_array_noise)
+            psd_signal = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+                frequency_array=frequencies, psd_array=psd_array_noise + psd_array_qpo)
+            psd_white_noise = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+                frequency_array=frequencies, psd_array=psd_array_white_noise)
+            psd_qpo = bilby.gw.detector.psd.PowerSpectralDensity.from_power_spectral_density_array(
+                frequency_array=frequencies, psd_array=psd_array_qpo)
+            plt.loglog()
+            plt.step(freqs[1:], powers[1:])
+            plt.plot(psd_signal.frequency_array, psd_signal.psd_array)
+            plt.xlim(freqs[1], freqs[-1])
+            plt.ylim(np.min(powers), powers[1])
+            plt.show()
+
+            ### Chi-squared tests
+            dofs = len(freqs) - 6
+            chi_square = QPOEstimation.model.psd.periodogram_chi_square_test(
+                frequencies=freqs, powers=powers,
+                psd=psd_signal, degrees_of_freedom=dofs)
+
+            print(f"chi-square entire segment: {chi_square}")
+
+            idxs = QPOEstimation.utils.get_indices_by_time(
+                freqs, minimum_time=central_frequency - 2*width, maximum_time=central_frequency + 2*width)
+
+            dofs = len(idxs) - 6
+            chi_square_qpo = QPOEstimation.model.psd.periodogram_chi_square_test(
+                frequencies=freqs[idxs], powers=powers[idxs],
+                psd=psd_signal, degrees_of_freedom=dofs)
+
+            print(f"chi-square QPO: {chi_square_qpo}")
+
+            frequency_break = (beta / white_noise) ** (1 / alpha)
+            idxs = QPOEstimation.utils.get_indices_by_time(freqs,
+                                                           minimum_time=0,
+                                                           maximum_time=frequency_break)
+            dofs = len(idxs) - 6
+            chi_square_red_noise = QPOEstimation.model.psd.periodogram_chi_square_test(
+                frequencies=freqs[idxs], powers=powers[idxs],
+                psd=psd_signal, degrees_of_freedom=dofs)
+            print(f"chi-square Red noise: {chi_square_red_noise}")
+        print()
+
+assert False
+
+
+
+
