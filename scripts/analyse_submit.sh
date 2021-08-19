c69e3d22efa9fa6f387786598d7b9190ad2eeffd
Moritz Huebner <email@moritz-huebner.de>
2021-08-19 20:18:53 +1000

    Final GRB runs

--- scripts/analyse_submit.sh
+++ scripts/analyse_submit.sh
@@ -4,7 +4,7 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --time=2:00:00
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=4G
 
 #srun python analyse.py --data_source ${1} --injection_id ${2} --injection_mode pure_qpo --recovery_mode pure_qpo --amplitude_min 10 --amplitude_max 100 --skewness_min 0.1 --skewness_max 10 --sigma_min 0.1 --sigma_max 1 --t_0_min 0 --t_0_max 3 --min_log_a -1 --max_log_a 1 --min_log_c -1 --max_log_c 1 --likelihood_model ${3} --background_model fred --n_components 1 --segment_length 3 --sampling_frequency 256 --band_minimum 1 --band_maximum 64 --plot True --nlive 500 --sample rwalk
 #srun python analyse.py --data_source grb --run_mode select_time--likelihood_model ${5} --data_mode normal --band_minimum 5 --band_maximum 64 --variance_stabilisation False --background_model gaussian --n_components ${6} --polynomial_max 1000 --segment_length ${7} --segment_step 0.2365 --sampling_frequency 256 --plot True --nlive 1000 --sample rslice --amplitude_min 0.001 --amplitude_max 1000 --offset True --offset_min 0 --offset_max 100 --try_load True
