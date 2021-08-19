3f477aad2a32bc246296dd8882ef0a71744f2258
Moritz Huebner <email@moritz-huebner.de>
2021-08-19 22:51:15 +1000

    Magnetar runs

--- scripts/analyse_super_submit.sh
+++ scripts/analyse_super_submit.sh
@@ -21,20 +21,9 @@
 
 
 ### GRB
-for model in red_noise general_qpo
-do
-  for n_components in {1..4}
-  do
-    sbatch analyse_submit.sh ${model} fred ${n_components}
-    sbatch analyse_submit.sh ${model} fred_norris ${n_components}
-    sbatch analyse_submit.sh ${model} fred_norris_extended ${n_components}
-    sbatch analyse_submit.sh ${model} skew_gaussian ${n_components}
-  done
-done
-
-#for model in red_noise general_qpo double_qpo
+#for model in red_noise general_qpo
 #do
-#  for n_components in {0..4}
+#  for n_components in {1..4}
 #  do
 #    sbatch analyse_submit.sh ${model} fred ${n_components}
 #    sbatch analyse_submit.sh ${model} fred_norris ${n_components}
@@ -42,6 +31,18 @@ done
 #    sbatch analyse_submit.sh ${model} skew_gaussian ${n_components}
 #  done
 #done
+
+### Magnetar flare
+for model in red_noise general_qpo double_qpo
+do
+  for n_components in {0..4}
+  do
+    sbatch analyse_submit.sh ${model} fred ${n_components}
+    sbatch analyse_submit.sh ${model} fred_norris ${n_components}
+    sbatch analyse_submit.sh ${model} fred_norris_extended ${n_components}
+    sbatch analyse_submit.sh ${model} skew_gaussian ${n_components}
+  done
+done
 
 #start_times=(4900 4920 4940 4960 4980)
 #end_times=(5100 5080 5060 5040 5020)
