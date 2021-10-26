63a886774f391d1b69e253200e668bfe29cbb8d8
Moritz Huebner <email@moritz-huebner.de>
2021-10-27 01:33:16 +1100

    some changes

--- scripts/analyse_periodogram_submit.sh
+++ scripts/analyse_periodogram_submit.sh
@@ -8,7 +8,7 @@
 
 
 # Injection
-srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_source injection --injection_id ${3} --injection_mode general_qpo --recovery_mode ${4} --window ${5} --run_mode select_time --injection_file_dir injection_files_pop --injection_likelihood_model whittle --likelihood_model whittle --frequency_mask_minimum ${6} --band_minimum ${7} --normalisation False --sample rslice --nlive 500 --try_load False --resume False
+#srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_source injection --injection_id ${3} --injection_mode general_qpo --recovery_mode ${4} --window ${5} --run_mode select_time --injection_file_dir injection_files_pop --injection_likelihood_model whittle --likelihood_model whittle --frequency_mask_minimum ${6} --band_minimum ${7} --normalisation False --sample rslice --nlive 500 --try_load False --resume False
 
 # Solar Flare
 #srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_source solar_flare --solar_flare_folder goes --solar_flare_id go1520130512 --recovery_mode ${3} --window hann --run_mode select_time --likelihood_model whittle --normalisation True --sample rwalk --nlive 1000 --try_load False --resume False
@@ -16,3 +16,6 @@ srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_sour
 
 #GRB
 #srun python analyse_periodogram.py --start_time ${1} --end_time ${2} --data_source grb --grb_id 090709A --grb_detector swift --grb_binning 64ms --band_minimum 0.05 --band_maximum 0.30 --recovery_mode ${3} --window ${4} --run_mode select_time --likelihood_model whittle --sample rslice --nlive 1000 --try_load False --resume False
+
+# Hares and hounds
+srun python analyse_periodogram.py --data_source hares_and_hounds --run_mode from_maximum  --hares_and_hounds_id ${1} --hares_and_hounds_round HH2  --likelihood_model whittle --recovery_mode ${2} --sample rwalk --nlive 2000 --use_ratio False --try_load False --resume False --plot True --offset True --jitter_term True --sampling_frequency 1
