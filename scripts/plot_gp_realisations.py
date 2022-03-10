a95f1799e5d598b32f4dd2544cd35a1441dc65e3
Moritz Huebner <email@moritz-huebner.de>
2022-03-10 15:46:29 +1100

    added gp realisation script

---
+++ scripts/plot_gp_realisations.py
@@ -0,0 +1,57 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import celerite
+
+import QPOEstimation
+
+# plt.style.use("paper.mplstyle")
+
+xs = np.linspace(-1, 1, 5000)
+yerr = np.zeros(len(xs))
+
+kernel_red_noise = QPOEstimation.likelihood.get_kernel(kernel_type="red_noise", jitter_term=False)
+kernel_qpo = QPOEstimation.likelihood.get_kernel(kernel_type="pure_qpo", jitter_term=False)
+kernel_qpo_plus_red_noise = QPOEstimation.likelihood.get_kernel(kernel_type="qpo_plus_red_noise", jitter_term=False)
+print(kernel_red_noise.parameter_vector)
+gp = celerite.GP(kernel=kernel_red_noise, mean=0)
+gp.compute(t=xs, yerr=yerr)
+yss = gp.sample(3)
+
+for ys in yss:
+    plt.plot(xs, ys)
+plt.xlabel("Time [s]")
+plt.ylabel("y")
+plt.savefig("realisations_red_noise.png")
+plt.show()
+
+print(kernel_qpo.parameter_names)
+print(kernel_qpo.parameter_vector)
+kernel_qpo.set_parameter(name="log_f", value=np.log(4))
+gp = celerite.GP(kernel=kernel_qpo, mean=0)
+gp.compute(t=xs, yerr=yerr)
+yss = gp.sample(3)
+
+for ys in yss:
+    plt.plot(xs, ys)
+plt.xlabel("Time [s]")
+plt.ylabel("y")
+plt.savefig("realisations_qpo.png")
+plt.show()
+
+
+
+kernel_qpo_plus_red_noise.set_parameter(name="terms[0]:log_f", value=np.log(4))
+kernel_qpo_plus_red_noise.set_parameter(name="terms[0]:log_a", value=0.0)
+kernel_qpo_plus_red_noise.set_parameter(name="terms[1]:log_a", value=2.5)
+gp = celerite.GP(kernel=kernel_qpo_plus_red_noise, mean=0)
+gp.compute(t=xs, yerr=yerr)
+yss = gp.sample(3)
+
+for ys in yss:
+    plt.plot(xs, ys)
+plt.xlabel("Time [s]")
+plt.ylabel("y")
+plt.savefig("realisations_qpo_plus_red_noise.png")
+plt.show()
