ec95fa91cf1d6e1a54789e548ba714b6ae251f61
Moritz Huebner <email@moritz-huebner.de>
2022-03-15 12:54:01 +1100

    Updated script

--- scripts/evaluate_identical_injections.py
+++ scripts/evaluate_identical_injections.py
@@ -4,25 +4,26 @@ import numpy as np
 from QPOEstimation.result import GPResult
 from QPOEstimation.utils import get_injection_outdir, get_injection_label
 
-# import matplotlib.pyplot as plt
-# plt.style.use("paper.mplstyle")
+plt.style.use("paper.mplstyle")
+
+base_injection_outdir="injections/injection_mss_with_mean"
 
 samples = []
 outdir_qpo_qpo = get_injection_outdir(
     injection_mode="qpo_plus_red_noise", recovery_mode="qpo_plus_red_noise",
-    likelihood_model="celerite", base_injection_outdir="injections/injection_mss")
+    likelihood_model="celerite", base_injection_outdir=base_injection_outdir)
 outdir_qpo_qpo = f"{outdir_qpo_qpo}/results"
 outdir_qpo_red_noise = get_injection_outdir(
     injection_mode="qpo_plus_red_noise", recovery_mode="red_noise",
-    likelihood_model="celerite", base_injection_outdir="injections/injection_mss")
+    likelihood_model="celerite", base_injection_outdir=base_injection_outdir)
 outdir_qpo_red_noise = f"{outdir_qpo_red_noise}/results"
 outdir_red_noise_red_noise = get_injection_outdir(
     injection_mode="red_noise", recovery_mode="red_noise",
-    likelihood_model="celerite", base_injection_outdir="injections/injection_mss")
+    likelihood_model="celerite", base_injection_outdir=base_injection_outdir)
 outdir_red_noise_red_noise = f"{outdir_red_noise_red_noise}/results"
 outdir_red_noise_qpo = get_injection_outdir(
     injection_mode="red_noise", recovery_mode="qpo_plus_red_noise",
-    likelihood_model="celerite", base_injection_outdir="injections/injection_mss")
+    likelihood_model="celerite", base_injection_outdir=base_injection_outdir)
 outdir_red_noise_qpo = f"{outdir_red_noise_qpo}/results"
 
 ln_bfs_qpo_inj = []
@@ -53,7 +54,7 @@ plt.xlabel("$\ln BF_{\mathrm{QPO}}$")
 plt.ylabel("$p(\ln BF_{\mathrm{QPO}})$")
 plt.legend()
 plt.tight_layout()
-plt.savefig("results/mss_plot_new.pdf")
+plt.savefig("results/mss_plot_with_mean.pdf")
 plt.show()
 
 print(len(np.where(np.array(ln_bfs_qpo_inj) > 0)[0]))
