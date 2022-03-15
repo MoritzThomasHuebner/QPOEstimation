f742b63dab1e8d7f40805d4942b409e5b888eb35
Moritz Huebner <email@moritz-huebner.de>
2022-03-15 14:26:32 +1100

    Updated script

--- scripts/evaluate_identical_injections.py
+++ scripts/evaluate_identical_injections.py
@@ -5,7 +5,7 @@ import numpy as np
 from QPOEstimation.result import GPResult
 from QPOEstimation.utils import get_injection_outdir, get_injection_label
 
-matplotlib.rcParams['text.usetex'] = True
+# matplotlib.rcParams['text.usetex'] = True
 plt.style.use("paper.mplstyle")
 
 base_injection_outdir="results/injection_files_mss_with_mean"
@@ -31,22 +31,26 @@ outdir_red_noise_qpo = f"{outdir_red_noise_qpo}/results"
 ln_bfs_qpo_inj = []
 ln_bfs_red_noise_inj = []
 
-for injection_id in range(0, 1000):
-    print(injection_id)
-    label = get_injection_label(run_mode="entire_segment", injection_id=injection_id) + "_1_skew_gaussians"
-    try:
-        res_qpo_qpo = GPResult.from_json(outdir=outdir_qpo_qpo, label=label)
-        res_qpo_red_noise = GPResult.from_json(outdir=outdir_qpo_red_noise, label=label)
-        res_red_noise_red_noise = GPResult.from_json(outdir=outdir_red_noise_red_noise, label=label)
-        res_red_noise_qpo = GPResult.from_json(outdir=outdir_red_noise_qpo, label=label)
-        ln_bfs_qpo_inj.append(res_qpo_qpo.log_evidence - res_qpo_red_noise.log_evidence)
-        ln_bfs_red_noise_inj.append(res_red_noise_qpo.log_evidence - res_red_noise_red_noise.log_evidence)
-    except (OSError, FileNotFoundError) as e:
-        print(e)
-        continue
+# for injection_id in range(0, 1000):
+#     print(injection_id)
+#     label = get_injection_label(run_mode="entire_segment", injection_id=injection_id) + "_1_skew_gaussians"
+#     try:
+#         res_qpo_qpo = GPResult.from_json(outdir=outdir_qpo_qpo, label=label)
+#         res_qpo_red_noise = GPResult.from_json(outdir=outdir_qpo_red_noise, label=label)
+#         res_red_noise_red_noise = GPResult.from_json(outdir=outdir_red_noise_red_noise, label=label)
+#         res_red_noise_qpo = GPResult.from_json(outdir=outdir_red_noise_qpo, label=label)
+#         ln_bfs_qpo_inj.append(res_qpo_qpo.log_evidence - res_qpo_red_noise.log_evidence)
+#         ln_bfs_red_noise_inj.append(res_red_noise_qpo.log_evidence - res_red_noise_red_noise.log_evidence)
+#     except (OSError, FileNotFoundError) as e:
+#         print(e)
+#         continue
 
-np.savetxt("results/ln_bfs_qpo_inj_mss.txt", ln_bfs_qpo_inj)
-np.savetxt("results/ln_bfs_red_noise_inj_mss.txt", ln_bfs_red_noise_inj)
+# np.savetxt("results/ln_bfs_qpo_inj_mss.txt", ln_bfs_qpo_inj)
+# np.savetxt("results/ln_bfs_red_noise_inj_mss.txt", ln_bfs_red_noise_inj)
+
+ln_bfs_qpo_inj = np.loadtxt("results/ln_bfs_qpo_inj_mss.txt")
+ln_bfs_red_noise_inj = np.loadtxt("results/ln_bfs_red_noise_inj_mss.txt")
+print(ln_bfs_red_noise_inj)
 bins = np.arange(-5, 25)
 # bins = "fd"
 plt.hist(ln_bfs_qpo_inj, alpha=0.5, density=True, bins=bins, label="Simulated red noise plus QPO")
@@ -57,7 +61,7 @@ plt.ylabel("$p(\ln BF_{\mathrm{QPO}})$")
 plt.legend()
 plt.tight_layout()
 plt.savefig("results/mss_plot_with_mean.pdf")
-plt.show()
+# plt.show()
 
 print(len(np.where(np.array(ln_bfs_qpo_inj) > 0)[0]))
 print(len(np.where(np.array(ln_bfs_red_noise_inj) > 0)[0]))
